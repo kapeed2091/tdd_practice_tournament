5137aeac5b9d5531203f8bbb639c9abf16e7d9b8
navya <navya.d@ibhubs.co>
2019-01-29 20:49:51 +0530

    test_create_tournament: compile error

---
+++ ib_tournament/tests/test_create_tournament.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+
+class TestCreateTournament(TestCase):
+
+    def test_create_tournament(self):
+        from ib_common.date_time_utils.get_current_local_date_time import \
+            get_current_local_date_time
+        from ib_common.date_time_utils.convert_datetime_to_local_string import \
+            convert_datetime_to_local_string
+        from ib_tournament.constants.general import DEFAULT_DATE_TIME_FORMAT
+
+        start_datetime = convert_datetime_to_local_string(
+            get_current_local_date_time(), DEFAULT_DATE_TIME_FORMAT)
+        total_rounds = 3
+
+        from ib_tournament.models import Tournament
+        initial_tournaments_count = Tournament.objects.count()
+        Tournament.create_tournament(total_rounds, start_datetime)
+        tournaments_count = Tournament.objects.count()
+
+        self.assertEqual(tournaments_count - initial_tournaments_count, 1)
