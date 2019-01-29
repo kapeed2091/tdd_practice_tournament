8b7e9eb7d358febf8a689b9d6e886c9006343025
Teja Sriram <teja@proyuga.tech>
2019-01-29 21:12:02 +0530

    create tournament - non-positive number of rounds - red state

--- tournament/tests/test_create_ko_tournament.py
+++ tournament/tests/test_create_ko_tournament.py
@@ -27,3 +27,27 @@ class TestCreateKOTournament(TestCase):
             start_datetime=start_datetime, status=status)
 
         self.assertEquals(input_tournament_details, tournament_details)
+
+    def testcase_non_positive_number_of_rounds(self):
+        import datetime
+        from tournament.models import KOTournament
+
+        user_id = 'user_1'
+        t_id = '1'
+        tournament_name = 'tournament_1'
+        number_of_rounds_case1 = 0
+        number_of_rounds_case2 = -1
+        start_datetime = datetime.datetime(2019, 1, 30, 15, 00, 00)
+        status = 'CAN_JOIN'
+
+        with self.assertRaises(Exception):
+            KOTournament.create_tournament(
+            user_id=user_id, t_id=t_id, name=tournament_name,
+            number_of_rounds=number_of_rounds_case1,
+            start_datetime=start_datetime, status=status)
+
+        with self.assertRaises(Exception):
+            KOTournament.create_tournament(
+            user_id=user_id, t_id=t_id, name=tournament_name,
+            number_of_rounds=number_of_rounds_case2,
+            start_datetime=start_datetime, status=status)
