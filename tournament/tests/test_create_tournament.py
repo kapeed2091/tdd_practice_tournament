ed358993e72906c28c6de7d48c167bf3b2d088f9
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-29 20:59:05 +0530

    TestCase: Create Tournament With Wrong User Id, State: Compile Error

--- tournament/tests/test_create_tournament.py
+++ tournament/tests/test_create_tournament.py
@@ -7,6 +7,11 @@ from django_swagger_utils.drf_server.exceptions import BadRequest
 
 class TestCreateTournament(TestCase):
     user_id = 'User'
+    invalid_user_id = 'Invalid_User'
+
+    def setUp(self):
+        from tournament.models import User
+        User.objects.create(user_id=self.user_id)
 
     def test_create_tournament(self):
         from tournament.models import Tournament
@@ -70,4 +75,17 @@ class TestCreateTournament(TestCase):
         }
 
         with self.assertRaisesMessage(BadRequest, 'Invalid start_datetime'):
+            Tournament.create_tournament(**tournament_request)
+
+    def test_create_tournament_with_wrong_user_id(self):
+        from tournament.models import Tournament
+        now = datetime.datetime.now()
+        start_datetime = now + datetime.timedelta(days=1)
+        tournament_request = {
+            "created_user_id": self.invalid_user_id,
+            "no_of_rounds": 3,
+            "start_datetime": start_datetime
+        }
+
+        with self.assertRaisesMessage(BadRequest, 'Invalid user_id'):
             Tournament.create_tournament(**tournament_request)
