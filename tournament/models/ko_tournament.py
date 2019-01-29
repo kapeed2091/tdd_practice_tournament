0893805c80bb2c6ae22f180a7d07200a7ba8aad5
Teja Sriram <teja@proyuga.tech>
2019-01-29 21:03:06 +0530

    Create tournament - refactoring - created global constants, changed model field names, one level of abstraction, vertical seperation

--- tournament/models/ko_tournament.py
+++ tournament/models/ko_tournament.py
@@ -2,20 +2,34 @@ from django.db import models
 
 
 class KOTournament(models.Model):
-    t_id = models.CharField(max_length=20)
-    name = models.CharField(max_length=30)
+    T_ID_MAX_LENGTH = 20
+    TOURNAMENT_NAME_MAX_LENGTH = 30
+    TOURNAMENT_STATUS_MAX_LENGTH = 30
+
+    t_id = models.CharField(max_length=T_ID_MAX_LENGTH)
+    name = models.CharField(max_length=TOURNAMENT_NAME_MAX_LENGTH)
     number_of_rounds = models.IntegerField()
     start_datetime = models.DateTimeField()
-    tournament_status = models.CharField(max_length=30)
+    status = models.CharField(max_length=TOURNAMENT_STATUS_MAX_LENGTH)
 
     @classmethod
-    def create_tournament(cls, user_id, id, name, number_of_rounds,
-                          start_datetime, tournament_status):
-        tournament = cls.objects.create(
-            t_id=id, name=name, number_of_rounds=number_of_rounds,
-            start_datetime=start_datetime, tournament_status=tournament_status)
+    def create_tournament(cls, user_id, t_id, name, number_of_rounds,
+                          start_datetime, status):
+        tournament = cls.create_tournament_in_db(
+            t_id=t_id, name=name, number_of_rounds=number_of_rounds,
+            start_datetime=start_datetime, status=status)
 
-        return {'id': tournament.t_id, 'name': tournament.name,
-                'number_of_rounds': tournament.number_of_rounds,
-                'start_datetime': tournament.start_datetime,
-                'tournament_status': tournament.tournament_status}
+        return tournament.convert_to_dict()
+
+    @classmethod
+    def create_tournament_in_db(cls, t_id, name, number_of_rounds,
+                                start_datetime, status):
+        return cls.objects.create(
+            t_id=t_id, name=name, number_of_rounds=number_of_rounds,
+            start_datetime=start_datetime, status=status)
+
+    def convert_to_dict(self):
+        return {'t_id': self.t_id, 'name': self.name,
+                'number_of_rounds': self.number_of_rounds,
+                'start_datetime': self.start_datetime,
+                'status': self.status}
--- tournament/tests/test_create_ko_tournament.py
+++ tournament/tests/test_create_ko_tournament.py
@@ -6,25 +6,24 @@ class TestCreateKOTournament(TestCase):
     def testcase_create_ko_tournament(self):
         import datetime
         user_id = 'user_1'
-        id = '1'
+        t_id = '1'
         tournament_name = 'tournament_1'
         number_of_rounds = 2
         start_datetime = datetime.datetime(2019, 1, 30, 15, 00, 00)
-        tournament_status = 'CAN_JOIN'
+        status = 'CAN_JOIN'
 
         input_tournament_details = {
-            'id': '1',
+            't_id': '1',
             'name': 'tournament_1',
             'number_of_rounds': 2,
             'start_datetime': datetime.datetime(2019, 1, 30, 15, 00, 00),
-            'tournament_status': 'CAN_JOIN'
+            'status': 'CAN_JOIN'
         }
 
         from tournament.models import KOTournament
         tournament_details = KOTournament.create_tournament(
-            user_id=user_id, id=id, name=tournament_name,
+            user_id=user_id, t_id=t_id, name=tournament_name,
             number_of_rounds=number_of_rounds,
-            start_datetime=start_datetime,
-            tournament_status=tournament_status)
+            start_datetime=start_datetime, status=status)
 
         self.assertEquals(input_tournament_details, tournament_details)
