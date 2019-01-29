c8955074be6c7cac106fb040da443242a2c1990e
navya <navya.d@ibhubs.co>
2019-01-29 19:44:56 +0530

    test_create_user: green state

--- tournament/models/player.py
+++ tournament/models/player.py
@@ -2,14 +2,15 @@ from django.db import models
 
 
 class Player(models.Model):
-    pass
+    username = models.CharField(max_length=50)
 
     @classmethod
     def create_player(cls, username):
-        return
+        cls.objects.create(username=username)
 
     @classmethod
     def get_player_dict(cls, username):
+        player = cls.objects.get(username=username)
         return {
-            'username': ''
+            'username': player.username
         }
