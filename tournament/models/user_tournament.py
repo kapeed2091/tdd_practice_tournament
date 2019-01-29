2b29a5412d92d3cbff88dc0fea6262c645a2dd79
Ratan Rithwik <rithwik@shirley.local>
2019-01-29 21:20:22 +0530

    green state: subscribe to tournament successful

--- tournament/models/user_tournament.py
+++ tournament/models/user_tournament.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class UserTournament(models.Model):
+    user_id = models.PositiveIntegerField()
+    tournament_id = models.PositiveIntegerField()
 
     @classmethod
     def subscribe_to_tournament(cls, user_id, tournament_id):
-        pass
+        cls.objects.create(
+            user_id=user_id,
+            tournament_id=tournament_id
+        )
