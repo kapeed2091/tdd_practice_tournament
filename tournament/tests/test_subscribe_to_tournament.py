c790fe6b56c47c1c78a19fa1a3dfb4511c02c8dd
Ratan Rithwik <rithwik@shirley.local>
2019-01-29 21:27:09 +0530

    Refactoring test case: green state

--- tournament/tests/test_subscribe_to_tournament.py
+++ tournament/tests/test_subscribe_to_tournament.py
@@ -4,14 +4,29 @@ from django.test import TestCase
 class TestSubscribeToTournament(TestCase):
 
     def testcase_subscribe_to_tournament(self):
-        from tournament.models import UserTournament
+        from tournament.models import User, Tournament, UserTournament
+        total_rounds = 4
+        start_datetime = "2019-12-12 13:00:00"
+        user_name = "John"
 
-        user_id = 1
-        tournament_id = 1
-        UserTournament.subscribe_to_tournament(user_id, tournament_id)
+        user = User.objects.create(name=user_name)
+
+        from ib_common.date_time_utils.convert_string_to_local_date_time \
+            import convert_string_to_local_date_time
+        date_time_format = '%Y-%m-%d %H:%M:%S'
+
+        start_datetime = convert_string_to_local_date_time(
+            start_datetime, date_time_format
+        )
+        tournament = Tournament.objects.create(
+            user_id=user.id,
+            total_rounds=total_rounds,
+            start_datetime=start_datetime
+        )
+        UserTournament.subscribe_to_tournament(user.id, tournament.id)
         user_tournaments = UserTournament.objects.all()
         user_tournament = user_tournaments[0]
 
         self.assertEqual(user_tournaments.count(), 1)
-        self.assertEqual(user_tournament.user_id, user_id)
-        self.assertEqual(user_tournament.tournament_id, tournament_id)
+        self.assertEqual(user_tournament.user_id, user.id)
+        self.assertEqual(user_tournament.tournament_id, tournament.id)
