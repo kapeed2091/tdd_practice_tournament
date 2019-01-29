87a7fe1f6621b9d68daff055c42b9d058e49a11f
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-29 20:37:53 +0530

    TestCase: Create Tournament With Start DateTime Less Than Now, State: Green

--- tournament/models/tournament.py
+++ tournament/models/tournament.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django_swagger_utils.drf_server.exceptions import BadRequest
 
@@ -11,7 +13,8 @@ class Tournament(models.Model):
 
     @classmethod
     def create_tournament(cls, created_user_id, no_of_rounds, start_datetime):
-        cls.validate_no_of_rounds(no_of_rounds)
+        cls.validate_request(no_of_rounds=no_of_rounds,
+                             start_datetime=start_datetime)
         tournament = cls.objects.create(
             created_user_id=created_user_id,
             no_of_rounds=no_of_rounds,
@@ -19,10 +22,21 @@ class Tournament(models.Model):
         )
         return tournament.convert_to_dict()
 
+    @classmethod
+    def validate_request(cls, no_of_rounds, start_datetime):
+        cls.validate_no_of_rounds(no_of_rounds)
+        cls.validate_start_datetime(start_datetime)
+
     @staticmethod
     def validate_no_of_rounds(no_of_rounds):
         if no_of_rounds <= 0:
             raise BadRequest('Invalid number of rounds')
+
+    @staticmethod
+    def validate_start_datetime(start_datetime):
+        now = datetime.datetime.now()
+        if start_datetime < now:
+            raise BadRequest('Invalid start_datetime')
 
     def convert_to_dict(self):
         return {
