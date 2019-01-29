5a7d561381732a477ed3b081626f4b8a9acfcd8b
Ratan Rithwik <rithwik@shirley.local>
2019-01-29 19:47:04 +0530

    Green state: create tournament succesful test case

--- tournament/models/tournament.py
+++ tournament/models/tournament.py
@@ -2,7 +2,18 @@ from django.db import models
 
 
 class Tournament(models.Model):
+    user_id = models.PositiveIntegerField()
+    total_rounds = models.PositiveIntegerField()
+    start_datetime = models.DateTimeField()
 
     @classmethod
     def create_tournament(cls, user_id, total_rounds, start_datetime):
-        pass
+        try:
+            obj = Tournament.objects.create(
+                user_id=user_id,
+                total_rounds=total_rounds,
+                start_datetime=start_datetime
+            )
+            return obj
+        except cls.DoesNotExist:
+            pass
