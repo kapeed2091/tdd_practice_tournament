1cd91793f329df40c4920bf441f59cc0437a18af
Ratan Rithwik <rithwik@shirley.local>
2019-01-29 19:43:59 +0530

    Red state: refactoring test case

--- tournament/tests/test_create_tournament.py
+++ tournament/tests/test_create_tournament.py
@@ -7,6 +7,14 @@ class TestCreateTournament(TestCase):
         user_id = 1
         total_rounds = 4
         start_datetime = "2019-12-12 13:00:00"
+
+        from ib_common.date_time_utils.convert_string_to_local_date_time \
+            import convert_string_to_local_date_time
+        date_time_format = '%Y-%m-%d %H:%M:%S'
+
+        start_datetime = convert_string_to_local_date_time(
+            start_datetime, date_time_format
+        )
         from tournament.models import Tournament
         Tournament.create_tournament(user_id, total_rounds, start_datetime)
 
