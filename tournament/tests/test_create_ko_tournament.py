8fb485b4b0d8335b76cd6b42901b7f722e72be0f
Teja Sriram <teja@proyuga.tech>
2019-01-29 20:04:48 +0530

    Updated Testcase - Create tournament - compile error state

--- tournament/tests/test_create_ko_tournament.py
+++ tournament/tests/test_create_ko_tournament.py
@@ -12,6 +12,14 @@ class TestCreateKOTournament(TestCase):
         start_datetime = datetime.datetime(2019, 1, 30, 15, 00, 00)
         tournament_status = 'CAN_JOIN'
 
+        input_tournament_details = {
+            'id': 1,
+            'name': 'tournament_1',
+            'number_of_rounds': 2,
+            'start_datetime': datetime.datetime(2019, 1, 30, 15, 00, 00),
+            'tournament_status': 'CAN_JOIN'
+        }
+
         from tournament.models import KOTournament
         tournament_details = KOTournament.create_tournament(
             user_id=user_id, id=id, name=tournament_name,
@@ -19,10 +27,4 @@ class TestCreateKOTournament(TestCase):
             start_datetime=start_datetime,
             tournament_status=tournament_status)
 
-        self.assertEquals(id, tournament_details['id'])
-        self.assertEquals(tournament_name, tournament_details['name'])
-        self.assertEquals(number_of_rounds, tournament_details[
-            'number_of_rounds'])
-        self.assertEquals(start_datetime, tournament_details['start_datetime'])
-        self.assertEquals(tournament_status, tournament_details[
-            'tournament_status'])
+        self.assertEquals(input_tournament_details, tournament_details)
