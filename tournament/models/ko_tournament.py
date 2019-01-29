0acfe79e4b6b148cd9059d0ef1a9c41993e1fff4
Teja Sriram <teja@proyuga.tech>
2019-01-29 20:10:12 +0530

    Create tournament - green state

--- tournament/models/ko_tournament.py
+++ tournament/models/ko_tournament.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class KOTournament(models.Model):
-    pass
 
     @classmethod
     def create_tournament(cls, user_id, id, name, number_of_rounds,
                           start_datetime, tournament_status):
-        pass
+        import datetime
+        return {'id': 1, 'name': 'tournament_1', 'number_of_rounds': 2,
+                'start_datetime': datetime.datetime(2019, 1, 30, 15, 00, 00),
+                'tournament_status': 'CAN_JOIN'}
