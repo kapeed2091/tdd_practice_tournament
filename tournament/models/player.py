d986c51eb094b75e8a3ad37e5e7a988e38c75643
navya <navya.d@ibhubs.co>
2019-01-29 20:01:54 +0530

    test_unique_username: green state

--- tournament/models/player.py
+++ tournament/models/player.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class Player(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
 
     @classmethod
     def create_player(cls, username):
+        cls._validate_unique_user(username)
         cls.objects.create(username=username)
+        return
+
+    @classmethod
+    def _validate_unique_user(cls, username):
+        from django_swagger_utils.drf_server.exceptions import BadRequest
+        from tournament.constants.exception_messages import USERNAME_EXISTS
+        if Player.objects.filter(username=username):
+            raise BadRequest(USERNAME_EXISTS)
+        return
 
     @classmethod
     def get_player(cls, username):
