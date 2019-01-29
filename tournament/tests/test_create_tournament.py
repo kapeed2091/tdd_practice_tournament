e6f3b9c48d349b27f397d9ccba6366d77cbea07f
Ratan Rithwik <rithwik@shirley.local>
2019-01-29 20:00:18 +0530

    Compilation error: test case invalid user_id

--- tournament/tests/test_create_tournament.py
+++ tournament/tests/test_create_tournament.py
@@ -25,3 +25,21 @@ class TestCreateTournament(TestCase):
         self.assertEqual(tournament_object.user_id, user_id)
         self.assertEqual(tournament_object.total_rounds, total_rounds)
         self.assertEqual(tournament_object.start_datetime, start_datetime)
+
+    def testcase_invalid_user_id(self):
+        user_id = 1
+        total_rounds = 4
+        start_datetime = "2019-12-12 13:00:00"
+
+        from ib_common.date_time_utils.convert_string_to_local_date_time \
+            import convert_string_to_local_date_time
+        date_time_format = '%Y-%m-%d %H:%M:%S'
+
+        start_datetime = convert_string_to_local_date_time(
+            start_datetime, date_time_format
+        )
+        from tournament.models import Tournament
+
+        from tournament.exceptions.exceptions import InvalidUserId
+        with self.assertRaises(InvalidUserId):
+            Tournament.create_tournament(user_id, total_rounds, start_datetime)
