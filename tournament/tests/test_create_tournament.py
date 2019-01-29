b1606e881a20c4f990bdb9d0fcb0ae0dcb25f227
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-29 19:42:34 +0530

    TestCase: Create Tournament, state: Compile Error

---
+++ tournament/tests/test_create_tournament.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+import datetime
+
+
+class TestCreateTournament(TestCase):
+    user_id = 'User'
+
+    def test_create_tournament(self):
+        from tournament.models import Tournament
+        now = datetime.datetime.now()
+        start_datetime = now + datetime.timedelta(days=1)
+        tournament_request = {
+            "created_user_id": self.user_id,
+            "no_of_rounds": 3,
+            "start_datetime": start_datetime
+        }
+        tournament_response = Tournament.create_tournament(
+            created_user_id=self.user_id,
+            no_of_rounds=3,
+            start_datetime=start_datetime)
+        self.assertEqual(tournament_request, tournament_response)
