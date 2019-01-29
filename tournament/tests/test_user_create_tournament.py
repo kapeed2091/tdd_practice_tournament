c2f66ec02bc155e90297068f00619b033591d11c
nandeep-tech <nandeep@ibtspl.com>
2019-01-29 19:33:44 +0530

    test_user_create_tournament compile error

---
+++ tournament/tests/test_user_create_tournament.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+
+class TestUserCreateTournament(TestCase):
+    username = "user1"
+
+    def test_user_create_tournament(self):
+        from datetime import datetime
+
+        no_of_rounds = 4
+        start_date_time = datetime.now()
+
+        tournament_details = Tournament.create_tournament(
+            rounds=no_of_rounds, start_date_time=start_date_time)
+
+        start_date_time_str = start_date_time.strftime(fmt="%Y-%m-%d")
+
+        self.assertEquals(start_date_time_str,
+                          tournament_details['start_datetime'])
+        self.assertEquals(no_of_rounds, tournament_details['no_of_rounds'])
