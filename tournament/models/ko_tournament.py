35b4d9b75ba7f6cf1895ba29a91483393d4e5bb7
Teja Sriram <teja@proyuga.tech>
2019-01-29 20:28:45 +0530

    Create tournament - green state with actual implementation (not refactored)

--- tournament/models/ko_tournament.py
+++ tournament/models/ko_tournament.py
@@ -2,11 +2,20 @@ from django.db import models
 
 
 class KOTournament(models.Model):
+    t_id = models.CharField(max_length=20)
+    name = models.CharField(max_length=30)
+    number_of_rounds = models.IntegerField()
+    start_datetime = models.DateTimeField()
+    tournament_status = models.CharField(max_length=30)
 
     @classmethod
     def create_tournament(cls, user_id, id, name, number_of_rounds,
                           start_datetime, tournament_status):
-        import datetime
-        return {'id': 1, 'name': 'tournament_1', 'number_of_rounds': 2,
-                'start_datetime': datetime.datetime(2019, 1, 30, 15, 00, 00),
-                'tournament_status': 'CAN_JOIN'}
+        tournament = cls.objects.create(
+            t_id=id, name=name, number_of_rounds=number_of_rounds,
+            start_datetime=start_datetime, tournament_status=tournament_status)
+
+        return {'id': tournament.t_id, 'name': tournament.name,
+                'number_of_rounds': tournament.number_of_rounds,
+                'start_datetime': tournament.start_datetime,
+                'tournament_status': tournament.tournament_status}
