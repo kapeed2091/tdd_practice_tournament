84add3aba02205de0c5ec01c94c954fbd8ca2cb4
Teja Sriram <teja@proyuga.tech>
2019-01-30 14:50:26 +0530

    list all tournaments - red state (minor change in testcase)

--- tournament/tests/test_list_of_all_tournaments.py
+++ tournament/tests/test_list_of_all_tournaments.py
@@ -11,6 +11,7 @@ class TestListAllTournaments(TestCase):
 
     def testcase_list_all_tournaments(self):
         from tournament.models import KOTournament, UserProfile
+        from ib_common.date_time_utils.get_current_local_date_time import get_current_local_date_time
         import datetime
         user_id = 'user_1'
         t_id_1 = '1'
@@ -18,24 +19,28 @@ class TestListAllTournaments(TestCase):
         tournament_name_1 = 'tournament_1'
         tournament_name_2 = 'tournament_2'
         number_of_rounds = 2
-        start_datetime_1 = datetime.datetime(2019, 1, 30, 15, 00, 00)
-        start_datetime_2 = datetime.datetime(2020, 1, 15, 15, 00, 00)
+        start_datetime_1 = get_current_local_date_time()+datetime.timedelta(hours=1)
+        start_datetime_2 = get_current_local_date_time()+datetime.timedelta(days=365)
         status = 'CAN_JOIN'
 
-        expected_all_tournaments = ({
+        expected_all_tournaments = list()
+
+        expected_output_1 = {
             't_id': '1',
             'name': 'tournament_1',
             'number_of_rounds': 2,
-            'start_datetime': datetime.datetime(2019, 1, 30, 15, 00, 00),
-            'status': 'CAN_JOIN'},
-            {
-                't_id': '2',
-                'name': 'tournament_2',
-                'number_of_rounds': 2,
-                'start_datetime': datetime.datetime(
-                    2020, 1, 15, 15, 00, 00),
-                'status': 'CAN_JOIN'
-            })
+            'start_datetime': start_datetime_1,
+            'status': 'CAN_JOIN'}
+
+        expected_output_2 = {
+            't_id': '2',
+            'name': 'tournament_2',
+            'number_of_rounds': 2,
+            'start_datetime': start_datetime_2,
+            'status': 'CAN_JOIN'
+        }
+        expected_all_tournaments.append(expected_output_1)
+        expected_all_tournaments.append(expected_output_2)
 
         UserProfile.objects.create(user_id=user_id)
         KOTournament.assign_t_id_to_tournament(
@@ -50,4 +55,6 @@ class TestListAllTournaments(TestCase):
 
         actual_all_tournaments = KOTournament.get_all_tournaments()
 
-        self.assertEquals(expected_all_tournaments, actual_all_tournaments)
+        import deepdiff
+        diff = deepdiff.DeepDiff(expected_all_tournaments, actual_all_tournaments)
+        self.assertEquals(diff, {})
