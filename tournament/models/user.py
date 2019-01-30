243e32f16ba734642c8247a1663e465f0adc8640
Ratan Rithwik <rithwik@shirley.local>
2019-01-30 09:33:45 +0530

    Refactoring: subscribe_to_tournament - invalid user id - G13 artificial coupling

--- tournament/models/user.py
+++ tournament/models/user.py
@@ -5,3 +5,10 @@ class User(models.Model):
     NAME_MAX_LENGTH = 20
 
     name = models.CharField(max_length=20)
+
+    @classmethod
+    def validate_user_id(cls, user_id):
+        user_exists = User.objects.filter(id=user_id).exists()
+        if not user_exists:
+            from ..exceptions.exceptions import InvalidUserId
+            raise InvalidUserId
--- tournament/models/user_tournament.py
+++ tournament/models/user_tournament.py
@@ -8,12 +8,10 @@ class UserTournament(models.Model):
     @classmethod
     def subscribe_to_tournament(cls, user_id, tournament_id):
         from ..models import User, Tournament
+
         Tournament.validate_tournament_id(tournament_id=tournament_id)
 
-        user_exists = User.objects.filter(id=user_id).exists()
-        if not user_exists:
-            from ..exceptions.exceptions import InvalidUserId
-            raise InvalidUserId
+        User.validate_user_id(user_id=user_id)
 
         cls.objects.create(
             user_id=user_id,
