cfe4175782c89a1083aa582156078b8592c3eba3
Teja Sriram <teja@proyuga.tech>
2019-01-30 17:25:42 +0530

    subscribe to tournament - tournament doesnot exist case - green state

--- tournament/models/tournament_user.py
+++ tournament/models/tournament_user.py
@@ -7,7 +7,11 @@ class TournamentUser(models.Model):
 
     @classmethod
     def subscribe_to_tournament(cls, user_id, tournament_id):
-        from tournament.models import UserProfile
+        from tournament.models import UserProfile, KOTournament
         UserProfile.is_registered_user(user_id=user_id)
+        try:
+            KOTournament.objects.get(t_id=tournament_id)
+        except:
+            raise Exception('Tournament doesnot exist')
 
         cls.objects.create(user_id=user_id, tournament_id=tournament_id)
--- tournament/tests/test_subscribe_to_tournament.py
+++ tournament/tests/test_subscribe_to_tournament.py
@@ -4,11 +4,21 @@ from django.test import TestCase
 class TestSubscribeToTournament(TestCase):
 
     def testcase_user_subscribe_to_tournament(self):
-        from tournament.models import TournamentUser, UserProfile
+        from tournament.models import TournamentUser, UserProfile, KOTournament
+        import datetime
+        from ib_common.date_time_utils.get_current_local_date_time \
+            import get_current_local_date_time
+
+        start_datetime = \
+            get_current_local_date_time()+datetime.timedelta(minutes=10)
         user_id = 'user_1'
         tournament_id = 'tournament_1'
 
         UserProfile.objects.create(user_id=user_id)
+        KOTournament.objects.create(
+            t_id=tournament_id, name='tournament_name_1',number_of_rounds=2,
+            start_datetime=start_datetime)
+
         old_count = len(TournamentUser.objects.filter(
             user_id=user_id, tournament_id=tournament_id))
 
