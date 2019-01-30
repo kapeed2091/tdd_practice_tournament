982bcdbfddf48b80c240bb037d9c3ba2e5911047
Teja Sriram <teja@proyuga.tech>
2019-01-30 10:51:13 +0530

    create tournament - tournaments with same_t_id - red state

--- tournament/tests/test_create_ko_tournament.py
+++ tournament/tests/test_create_ko_tournament.py
@@ -116,3 +116,31 @@ class TestCreateKOTournament(TestCase):
                 user_id=user_id, t_id=t_id, name=tournament_name,
                 number_of_rounds=number_of_rounds,
                 start_datetime=start_datetime, status=status)
+
+    def testcase_create_multiple_tournaments_with_same_t_id(self):
+        import datetime
+        from tournament.models import KOTournament, UserProfile
+
+        user_id = 'non_user_1'
+        t_id = '1'
+        tournament_name_1 = 'tournament_1'
+        tournament_name_2 = 'tournament_2'
+        number_of_rounds_1 = 2
+        number_of_rounds_2 = 3
+        start_datetime = datetime.datetime(2020, 1, 30, 05, 00, 00)
+        status = 'CAN_JOIN'
+
+        UserProfile.objects.create(user_id=user_id)
+
+        tournament_details_1 = KOTournament.create_tournament(
+            user_id=user_id, t_id=t_id, name=tournament_name_1,
+            number_of_rounds=number_of_rounds_1,
+            start_datetime=start_datetime, status=status)
+
+        with self.assertRaisesMessage(
+                Exception,
+                expected_message='Tournaments with same t_id cannot be created'):
+            KOTournament.create_tournament(
+                user_id=user_id, t_id=t_id, name=tournament_name_2,
+                number_of_rounds=number_of_rounds_2,
+                start_datetime=start_datetime, status=status)
