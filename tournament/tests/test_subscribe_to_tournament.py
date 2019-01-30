22e0c58d3231d3c061d57b4939233d14e1c1e5b4
Teja Sriram <teja@proyuga.tech>
2019-01-30 20:04:44 +0530

    subscribe to tournament - user can't join with invalid status - red state

--- tournament/tests/test_subscribe_to_tournament.py
+++ tournament/tests/test_subscribe_to_tournament.py
@@ -68,3 +68,26 @@ class TestSubscribeToTournament(TestCase):
                 Exception, expected_message='Tournament has started'):
             TournamentUser.subscribe_to_tournament(user_id=user_id,
                                                    tournament_id=tournament_id)
+
+    def testcase_subscribe_to_tournament_wrong_status(self):
+        from tournament.models import TournamentUser, UserProfile, \
+            KOTournament
+        import datetime
+        from ib_common.date_time_utils.get_current_local_date_time \
+            import get_current_local_date_time
+
+        start_datetime = \
+            get_current_local_date_time() + datetime.timedelta(minutes=10)
+        user_id = 'user_1'
+        tournament_id = 'tournament_1'
+
+        UserProfile.objects.create(user_id=user_id)
+        KOTournament.objects.create(
+            t_id=tournament_id, name='tournament_name_1',
+            number_of_rounds=2,
+            start_datetime=start_datetime, status='IN_PROGRESS')
+
+        with self.assertRaisesMessage(
+                Exception, expected_message='Invalid Tournament Status'):
+            TournamentUser.subscribe_to_tournament(
+                user_id=user_id, tournament_id=tournament_id)
