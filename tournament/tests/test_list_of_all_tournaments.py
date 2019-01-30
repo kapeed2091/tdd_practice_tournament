c7c9709627b31ceba382a5e4c7ec77c934ed4516
Teja Sriram <teja@proyuga.tech>
2019-01-30 15:31:06 +0530

    list all tournaments - for non-registered user - red state (but wrong red state, actually extra argument is passed)

--- tournament/tests/test_list_of_all_tournaments.py
+++ tournament/tests/test_list_of_all_tournaments.py
@@ -62,3 +62,37 @@ class TestListAllTournaments(TestCase):
         diff = deepdiff.DeepDiff(expected_all_tournaments,
                                  actual_all_tournaments)
         self.assertEquals(diff, {})
+
+    def test_list_tournaments_for_non_registered_user(self):
+        from tournament.models import KOTournament, UserProfile
+        from ib_common.date_time_utils.get_current_local_date_time import \
+            get_current_local_date_time
+        import datetime
+
+        user_id = 'user_1'
+        non_registered_user_id = 'non_user_1'
+        t_id_1 = '1'
+        t_id_2 = '2'
+        tournament_name_1 = 'tournament_1'
+        tournament_name_2 = 'tournament_2'
+        number_of_rounds = 2
+        start_datetime_1 = get_current_local_date_time() + \
+                           datetime.timedelta(hours=1)
+        start_datetime_2 = get_current_local_date_time() + \
+                           datetime.timedelta(days=365)
+        status = 'CAN_JOIN'
+
+        UserProfile.objects.create(user_id=user_id)
+        KOTournament.assign_t_id_to_tournament(
+            t_id=t_id_1, name=tournament_name_1,
+            number_of_rounds=number_of_rounds,
+            start_datetime=start_datetime_1, status=status)
+
+        KOTournament.assign_t_id_to_tournament(
+            t_id=t_id_2, name=tournament_name_2,
+            number_of_rounds=number_of_rounds,
+            start_datetime=start_datetime_2, status=status)
+
+        with self.assertRaisesMessage(Exception,
+                                      expected_message='User not registered'):
+            KOTournament.get_all_tournaments(user_id=non_registered_user_id)
