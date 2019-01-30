fced33c8ba084dc37338cc04f5ec58fce1bd58fb
Ratan Rithwik <rithwik@shirley.local>
2019-01-30 09:16:13 +0530

    green State: subscribe to tournament invlaid tournament id

--- tournament/models/user_tournament.py
+++ tournament/models/user_tournament.py
@@ -7,6 +7,14 @@ class UserTournament(models.Model):
 
     @classmethod
     def subscribe_to_tournament(cls, user_id, tournament_id):
+        from ..models import Tournament
+        tournament_exists = Tournament.objects.filter(
+            id=tournament_id).exists()
+
+        if not tournament_exists:
+            from ..exceptions.exceptions import InvalidTournamentId
+            raise InvalidTournamentId
+
         cls.objects.create(
             user_id=user_id,
             tournament_id=tournament_id
