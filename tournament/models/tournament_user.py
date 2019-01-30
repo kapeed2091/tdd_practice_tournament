5efbb74eb1cd6570b8d91f772c3a6938c1a9d345
nandeep-tech <nandeep@ibtspl.com>
2019-01-30 16:27:07 +0530

    test_user_already_subscribed green state

--- tournament/models/tournament_user.py
+++ tournament/models/tournament_user.py
@@ -8,6 +8,16 @@ class TournamentUser(models.Model):
     @classmethod
     def subscribe_user_to_tournament(cls, tournament_id, username):
         from .user import User
+        cls.validate_user_already_subscribed(
+            tournament_id=tournament_id, username=username)
         user_id = User.get_user_id(username=username)
 
         cls.objects.create(user_id=user_id, tournament_id=tournament_id)
+
+    @classmethod
+    def validate_user_already_subscribed(cls, tournament_id, username):
+        try:
+            cls.objects.get(tournament_id=tournament_id, user__username=username)
+            raise Exception("User already subscribed to given tournament")
+        except cls.DoesNotExist:
+            pass
