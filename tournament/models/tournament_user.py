5475317e704503551a3fd09eff0fd500307d5e25
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-30 17:41:16 +0530

    TestCase: Subscribe To Tournament Which Has Started, state: Green

--- tournament/models/tournament_user.py
+++ tournament/models/tournament_user.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django_swagger_utils.drf_server.exceptions import Forbidden
+
 from tournament.models import User, KoTournament
+from tournament.utils.date_time_utils import get_current_date_time
 
 
 class TournamentUser(models.Model):
@@ -13,5 +16,11 @@ class TournamentUser(models.Model):
 
         user = User.get_user(user_id)
         tournament = KoTournament.get_tournament(tournament_id)
+        cls.validate_tournament_start_datetime(tournament)
         cls.objects.create(user=user, tournament=tournament)
 
+    @staticmethod
+    def validate_tournament_start_datetime(tournament):
+        now = get_current_date_time()
+        if tournament.start_datetime <= now:
+            raise Forbidden('Subscription can only be done before starting of the Tournament')
