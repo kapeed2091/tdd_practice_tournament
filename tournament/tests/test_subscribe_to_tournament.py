9c4be08701267430628122072b188a83b5dc1e6c
Ratan Rithwik <rithwik@shirley.local>
2019-01-30 10:04:44 +0530

    Compilation error: test case user already registered

--- tournament/tests/test_subscribe_to_tournament.py
+++ tournament/tests/test_subscribe_to_tournament.py
@@ -66,3 +66,34 @@ class TestSubscribeToTournament(TestCase):
         from tournament.exceptions.exceptions import InvalidUserId
         with self.assertRaises(InvalidUserId):
             UserTournament.subscribe_to_tournament(user_id, tournament.id)
+
+    def test_case_user_already_registered(self):
+        from tournament.models import User, Tournament, UserTournament
+        total_rounds = 4
+        start_datetime = "2019-12-12 13:00:00"
+        user_name = "John"
+
+        user = User.objects.create(name=user_name)
+
+        from ib_common.date_time_utils.convert_string_to_local_date_time \
+            import convert_string_to_local_date_time
+        date_time_format = '%Y-%m-%d %H:%M:%S'
+
+        start_datetime = convert_string_to_local_date_time(
+            start_datetime, date_time_format
+        )
+        tournament = Tournament.objects.create(
+            user_id=user.id,
+            total_rounds=total_rounds,
+            start_datetime=start_datetime
+        )
+
+        UserTournament.objects.create(
+            user_id=user.id,
+            tournament_id=tournament.id
+        )
+        UserTournament.subscribe_to_tournament(user.id, tournament.id)
+
+        from tournament.exceptions.exceptions import UserAlreadyRegistered
+        with self.assertRaises(UserAlreadyRegistered):
+            UserTournament.subscribe_to_tournament(user.id, tournament.id)
