83048d9a90643b3ef212973038673f46075f5a21
Ratan Rithwik <rithwik@shirley.local>
2019-01-30 09:31:30 +0530

    green state: subscribe_to_tournament - invalid user id

--- tournament/models/user_tournament.py
+++ tournament/models/user_tournament.py
@@ -7,8 +7,13 @@ class UserTournament(models.Model):
 
     @classmethod
     def subscribe_to_tournament(cls, user_id, tournament_id):
-        from ..models import Tournament
+        from ..models import User, Tournament
         Tournament.validate_tournament_id(tournament_id=tournament_id)
+
+        user_exists = User.objects.filter(id=user_id).exists()
+        if not user_exists:
+            from ..exceptions.exceptions import InvalidUserId
+            raise InvalidUserId
 
         cls.objects.create(
             user_id=user_id,
