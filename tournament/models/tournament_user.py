65926198a203ee001fb3e041bd4fd804e4079e35
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-30 19:33:12 +0530

    TestCase: Subscribe To Tournament After Reaching Max Memebers, State: Green

--- tournament/models/tournament_user.py
+++ tournament/models/tournament_user.py
@@ -16,11 +16,25 @@ class TournamentUser(models.Model):
 
         user = User.get_user(user_id)
         tournament = KoTournament.get_tournament(tournament_id)
-        cls.validate_tournament_start_datetime(tournament)
+        cls.validate_subscribe_request(tournament)
         cls.objects.create(user=user, tournament=tournament)
+
+    @classmethod
+    def validate_subscribe_request(cls, tournament):
+        cls.validate_tournament_start_datetime(tournament)
+        cls.validate_max_members(tournament)
 
     @staticmethod
     def validate_tournament_start_datetime(tournament):
         now = get_current_date_time()
         if tournament.start_datetime <= now:
             raise Forbidden('Subscription can only be done before starting of the Tournament')
+
+    @classmethod
+    def validate_max_members(cls, tournament):
+        no_of_rounds = tournament.no_of_rounds
+        max_members_allowed = 2 ** no_of_rounds
+
+        no_of_users = cls.objects.filter(tournament=tournament).count()
+        if no_of_users == max_members_allowed:
+            raise Forbidden('There is no place for new subscriptions')
