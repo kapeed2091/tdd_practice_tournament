6611485805e78818eaa78d827c5cdbbe9f4ba84a
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-31 16:58:20 +0530

    TestCase: Play Match, State: Green

--- tournament/models/match.py
+++ tournament/models/match.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from tournament.constants.general import MatchStatus
 from tournament.models import User
 from tournament.models import KoTournament
 
@@ -13,4 +15,16 @@ class Match(models.Model):
 
     @classmethod
     def play_match(cls, user_id, match_id):
-        pass
+        match = cls.get_match(user_id=user_id, match_id=match_id)
+        match.update_match_status(status=MatchStatus.IN_PROGRESS.value)
+
+    @classmethod
+    def get_match(cls, user_id, match_id):
+        from tournament.models import User
+
+        user = User.get_user(user_id)
+        return cls.objects.get(user=user, id=match_id)
+
+    def update_match_status(self, status):
+        self.status = status
+        self.save()
