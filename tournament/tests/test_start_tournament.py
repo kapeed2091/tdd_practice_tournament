868617c7009eb9dca1de43647d6eb82b16d46a5c
nandeep-tech <nandeep@ibtspl.com>
2019-01-31 16:41:08 +0530

    test_start_tournament_before_start_datetime green state

--- tournament/tests/test_start_tournament.py
+++ tournament/tests/test_start_tournament.py
@@ -4,6 +4,10 @@ from tdd_practice.constants.general import TournamentStatus
 
 
 class TestStartTournament(TestCase):
+    from ib_common.date_time_utils.get_current_local_date_time \
+        import get_current_local_date_time
+
+    start_datetime = get_current_local_date_time()
     tournament_status = TournamentStatus.CAN_JOIN.value
     tournament = None
 
@@ -23,16 +27,32 @@ class TestStartTournament(TestCase):
                           tournament.status)
         self.assertEquals(self._calculate_no_matches(), no_matches)
 
-    def _create_tournament(self):
+    def test_start_tournament_before_start_datetime(self):
         from tournament.models import Tournament
-        from ib_common.date_time_utils.get_current_local_date_time \
-            import get_current_local_date_time
+        from datetime import timedelta
 
-        curr_datetime = get_current_local_date_time()
+        self.start_datetime = self.start_datetime + timedelta(days=1)
+        self._create_tournament()
+        Tournament.start_tournament(tournament_id=self.tournament.id)
+
+        from tournament.models import Tournament
+        tournament = Tournament.objects.get(id=self.tournament.id)
+
+        from tournament.models.tournament_match import TournamentMatch
+        no_matches = \
+            TournamentMatch.objects.filter(tournament_id=tournament.id).count()
+
+        self.assertEquals(TournamentStatus.CAN_JOIN.value,
+                          tournament.status)
+        self.assertEquals(0, no_matches)
+
+    def _create_tournament(self, ):
+        from tournament.models import Tournament
+
         self.tournament = Tournament.objects.create(
             name="Knock Out",
             no_of_rounds=3,
-            start_datetime=curr_datetime,
+            start_datetime=self.start_datetime,
             status=self.tournament_status)
 
     def _calculate_no_matches(self):
