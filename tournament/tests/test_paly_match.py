4c071ce3db07bb06db9c760687dad28f15c2f298
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-31 17:34:31 +0530

    TestCase: Play Match With Out Starting Tournament, State: Red

--- tournament/tests/test_paly_match.py
+++ tournament/tests/test_paly_match.py
@@ -1,5 +1,6 @@
 import datetime
 from django.test import TestCase
+from django_swagger_utils.drf_server.exceptions import Forbidden
 from ib_common.date_time_utils.get_current_datetime import get_current_datetime
 
 from tournament.constants.general import TournamentStatus, MatchStatus
@@ -22,9 +23,23 @@ class TestPlayMatch(TestCase):
             start_datetime=now - datetime.timedelta(days=1),
             status=TournamentStatus.IN_PROGRESS.value
         )
+        tournament2 = KoTournament.objects.create(
+            created_user_id='User',
+            name='Tournament2',
+            no_of_rounds=2,
+            start_datetime=now + datetime.timedelta(days=1),
+            status=TournamentStatus.YET_TO_START.value
+        )
+
         Match.objects.create(
             user=user1,
             tournament=tournament,
+            status=MatchStatus.YET_TO_START.value
+        )
+
+        Match.objects.create(
+            user=user1,
+            tournament=tournament2,
             status=MatchStatus.YET_TO_START.value
         )
 
@@ -34,5 +49,11 @@ class TestPlayMatch(TestCase):
         Match.play_match(user_id=self.user1_id, match_id=1)
 
         user1 = User.objects.get(user_id=self.user1_id)
-        match = Match.objects.get(user=user1)
+        match = Match.objects.get(user=user1, id=1)
         self.assertEqual(match.status, MatchStatus.IN_PROGRESS.value)
+
+    def test_play_match_with_out_starting_tournament(self):
+        from tournament.models import Match
+
+        with self.assertRaisesMessage(Forbidden, 'Match can be played only after the tournament has started'):
+            Match.play_match(user_id=self.user1_id, match_id=2)
