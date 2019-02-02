55a573212072cec39cfcb43cb172f70fd988e725
Teja Sriram <teja@proyuga.tech>
2019-02-02 17:05:24 +0530

    create match - any two matches cannot have same match id when created - green state

--- tournament/models/tournament_match.py
+++ tournament/models/tournament_match.py
@@ -6,6 +6,7 @@ class TournamentMatch(models.Model):
     t_id = models.CharField(max_length=T_ID_MAX_LENGTH)
     player_one = models.CharField(max_length=USER_ID_MAX_LENGTH)
     player_two = models.CharField(max_length=USER_ID_MAX_LENGTH)
+    match_id = models.CharField(max_length=20)
 
     @classmethod
     def create_match(cls, request_data):
@@ -25,4 +26,11 @@ class TournamentMatch(models.Model):
 
     @classmethod
     def assign_match_id(cls, request_data, match_id):
-        pass
+        if cls.objects.filter(match_id=match_id).exists():
+            raise Exception('Match id already assigned to another match')
+        else:
+            cls.objects.create(
+                t_id=request_data['tournament_id'],
+                player_one=request_data['player_one_user_id'],
+                player_two=request_data['player_two_user_id'],
+                match_id=match_id)
