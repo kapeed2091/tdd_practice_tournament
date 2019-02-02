a858b3f47f446356276846d4738c69a0d8881d92
Teja Sriram <teja@proyuga.tech>
2019-02-02 16:56:34 +0530

    create match - any two matches cannot have same match id when created - compile error

--- tournament/tests/test_create_tournament_matches.py
+++ tournament/tests/test_create_tournament_matches.py
@@ -212,3 +212,49 @@ class TestCreateTournamentMatches(TestCase):
                 Exception,
                 expected_message='User(s) not subscribed to Tournament'):
             TournamentMatch.create_match(request_data=create_match_request)
+
+    def testcase_cannot_create_matches_with_same_unique_id(self):
+        from tournament.models import TournamentMatch, KOTournament, \
+            UserProfile, TournamentUser
+        from ib_common.date_time_utils.get_current_local_date_time import \
+            get_current_local_date_time
+        from datetime import timedelta
+        from tournament.constants import TournamentStatus
+
+        tournament_id = 'tournament_1'
+        player_one_user_id = 'user_1'
+        player_two_user_id = 'user_2'
+        tournament_name = 'city_tournament_1'
+        number_of_rounds = 2
+        start_datetime_past = \
+            get_current_local_date_time() - timedelta(minutes=5)
+        match_id = '1234'
+
+        create_match_request = {
+            'tournament_id': tournament_id,
+            'player_one_user_id': player_one_user_id,
+            'player_two_user_id': player_two_user_id
+        }
+
+        UserProfile.objects.create(user_id=player_one_user_id)
+        UserProfile.objects.create(user_id=player_two_user_id)
+
+        TournamentUser.objects.create(t_id=tournament_id,
+                                      user_id=player_one_user_id)
+        TournamentUser.objects.create(t_id=tournament_id,
+                                      user_id=player_two_user_id)
+
+        KOTournament.objects.create(
+            t_id=tournament_id, name=tournament_name,
+            number_of_rounds=number_of_rounds,
+            start_datetime=start_datetime_past,
+            status=TournamentStatus.FULL_YET_TO_START.value)
+
+        TournamentMatch.assign_match_id(request_data=create_match_request,
+                                        match_id=match_id)
+
+        with self.assertRaisesMessage(
+                Exception,
+                expected_message='Match id already assigned to another match'):
+            TournamentMatch.assign_match_id(request_data=create_match_request,
+                                            match_id=match_id)
