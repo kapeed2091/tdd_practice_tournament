0581edcb6d62f5b6d11c75f0d48c78f944c3520a
Ratan Rithwik <rithwik@shirley.local>
2019-02-02 10:21:27 +0530

    red state: create match successful case

--- tournament/models/__init__.py
+++ tournament/models/__init__.py
@@ -1,9 +1,11 @@
 from .tournament import Tournament
 from .user import User
 from .user_tournament import UserTournament
+from .match import Match
 
 __all__ = [
     'Tournament',
     'User',
-    'UserTournament'
+    'UserTournament',
+    'Match'
 ]
---
+++ tournament/models/match.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Match(models.Model):
+    tournament_id = models.PositiveIntegerField()
+    round_number = models.PositiveIntegerField()
+
+    @classmethod
+    def create_match(cls, tournament_id, round_number):
+        pass
