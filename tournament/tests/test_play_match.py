84b597629aafa0b7922372231ae7621d78afc030
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-02-02 17:39:12 +0530

    Refactoring: Test Play Match, Reasons: Decouple setup for each test case

--- tournament/tests/test_play_match.py
+++ tournament/tests/test_play_match.py
@@ -17,49 +17,9 @@ class TestPlayMatch(TestCase):
     match2_id = 'Match2'
     invalid_match_id = 'InvalidMatch'
 
-    def setUp(self):
-        from tournament.models import User, KoTournament, Match
-
-        now = get_current_datetime()
-        user1 = User.objects.create(
-            user_id=self.user1_id
-        )
-
-        User.objects.create(
-            user_id=self.user2_id
-        )
-
-        tournament1 = KoTournament.objects.create(
-            created_user_id=self.user_id,
-            name='Tournament1',
-            no_of_rounds=2,
-            start_datetime=now - datetime.timedelta(days=1),
-            status=TournamentStatus.IN_PROGRESS.value
-        )
-
-        tournament2 = KoTournament.objects.create(
-            created_user_id=self.user_id,
-            name='Tournament2',
-            no_of_rounds=2,
-            start_datetime=now + datetime.timedelta(days=1),
-            status=TournamentStatus.YET_TO_START.value
-        )
-
-        Match.objects.create(
-            match_id=self.match1_id,
-            user=user1,
-            tournament=tournament1,
-            status=MatchStatus.YET_TO_START.value
-        )
-
-        Match.objects.create(
-            match_id=self.match2_id,
-            user=user1,
-            tournament=tournament2,
-            status=MatchStatus.YET_TO_START.value
-        )
-
     def test_user_will_play_match(self):
+        self.setup_user_will_play_match()
+
         from tournament.models import User, Match
 
         user1 = User.objects.get(user_id=self.user1_id)
@@ -71,11 +31,27 @@ class TestPlayMatch(TestCase):
         match = Match.objects.get(user=user1, match_id=self.match1_id)
         self.assertEqual(match.status, MatchStatus.IN_PROGRESS.value)
 
+    def setup_user_will_play_match(self):
+        self.create_user_tournament_match(
+            user_id=self.user1_id,
+            tournament_status=TournamentStatus.IN_PROGRESS.value,
+            match_id=self.match1_id
+        )
+
     def test_user_try_to_play_match_where_tournament_is_not_started(self):
+        self.setup_user_try_to_play_match_where_tournament_is_not_started()
+
         from tournament.models import Match
 
         with self.assertRaisesMessage(Forbidden, 'Match can be played only after the tournament has started'):
             Match.play_match(user_id=self.user1_id, match_id=self.match2_id)
+
+    def setup_user_try_to_play_match_where_tournament_is_not_started(self):
+        self.create_user_tournament_match(
+            user_id=self.user1_id,
+            tournament_status=TournamentStatus.YET_TO_START.value,
+            match_id=self.match2_id
+        )
 
     def test_play_match_with_invalid_user(self):
         from tournament.models import Match
@@ -84,13 +60,57 @@ class TestPlayMatch(TestCase):
             Match.play_match(user_id=self.invalid_user_id, match_id=self.match2_id)
 
     def test_play_match_with_invalid_match_id(self):
+        self.setup_play_match_with_invalid_match_id()
+
         from tournament.models import Match
 
         with self.assertRaisesMessage(NotFound, 'Match does not exist with the given match id'):
             Match.play_match(user_id=self.user1_id, match_id=self.invalid_match_id)
 
+    def setup_play_match_with_invalid_match_id(self):
+        from tournament.models import User
+
+        User.objects.create(
+            user_id=self.user1_id
+        )
+
     def test_play_match_user_does_not_belong_to_match(self):
+        self.setup_play_match_user_does_not_belong_to_match()
+
         from tournament.models import Match
 
         with self.assertRaisesMessage(Forbidden, 'User does not belong to the match'):
             Match.play_match(user_id=self.user2_id, match_id=self.match1_id)
+
+    def setup_play_match_user_does_not_belong_to_match(self):
+        self.create_user_tournament_match(
+            user_id=self.user1_id,
+            tournament_status=TournamentStatus.IN_PROGRESS.value,
+            match_id=self.match1_id
+        )
+
+        from tournament.models import User
+        User.objects.create(
+            user_id=self.user2_id
+        )
+
+    def create_user_tournament_match(self, user_id, tournament_status, match_id):
+        from tournament.models import User, KoTournament, Match
+
+        now = get_current_datetime()
+        user = User.objects.create(
+            user_id=user_id
+        )
+        tournament = KoTournament.objects.create(
+            created_user_id=self.user_id,
+            name='Tournament',
+            no_of_rounds=2,
+            start_datetime=now + datetime.timedelta(days=1),
+            status=tournament_status
+        )
+        Match.objects.create(
+            match_id=match_id,
+            user=user,
+            tournament=tournament,
+            status=MatchStatus.YET_TO_START.value
+        )
