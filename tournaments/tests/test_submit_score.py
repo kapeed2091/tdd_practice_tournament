9adb2fc3e21c1a217d80e1592406bdd33237175b
Ratan Rithwik <rithwik@shirley.local>
2019-02-02 16:03:22 +0530

    compilation error: invalid score

--- tournaments/tests/test_submit_score.py
+++ tournaments/tests/test_submit_score.py
@@ -27,3 +27,25 @@ class TestSubmitScore(TestUtils):
             user_id=user.id, match_id=match.id
         )
         self.assertEqual(user_match_obj.score, score)
+
+    def test_invalid_score(self):
+        user = self.create_user()
+        tournament = self.create_tournament(user_id=user.id)
+
+        self.create_user_tournament(
+            user_id=user.id, tournament_id=tournament.id
+        )
+        round_number = 2
+        match = self.create_match(
+            tournament_id=tournament.id, round_number=round_number
+        )
+
+        user_match = self.create_user_match(
+            user_id=user.id, match_id=match.id
+        )
+
+        score = -100
+
+        from tournaments.exceptions.custom_exceptions import InvalidScore
+        with self.assertRaises(InvalidScore):
+            user_match.submit_score(score=score)
