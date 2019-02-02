3aa8abab2dc585b3d386483f7adfd7a0a17e17e0
Ratan Rithwik <rithwik@shirley.local>
2019-02-02 20:39:21 +0530

    compilation error: level up

---
+++ tournaments/tests/test_level_up.py
@@ -0,0 +1,41 @@
+from .test_utils import TestUtils
+
+
+class TestLevelUp(TestUtils):
+
+    def test_level_up(self):
+        user_1 = self.create_user()
+        user_2 = self.create_user(name="John-2")
+
+        tournament = self.create_tournament(user_id=user_1.id)
+
+        self.create_user_tournament_(
+            user_id=user_1.id, tournament_id=tournament.id
+        )
+        self.create_user_tournament_(
+            user_id=user_2.id, tournament_id=tournament.id
+        )
+
+        round_number = 2
+        match = self.create_match(
+            tournament_id=tournament.id, round_number=round_number
+        )
+
+        self.create_user_match(
+            user_id=user_1.id, match_id=match.id, score=100
+        )
+
+        self.create_user_match(
+            user_id=user_2.id, match_id=match.id, score=200
+        )
+
+        from tournaments.models import UserTournament
+        UserTournament.level_up(
+            user_id=user_2.id, match_id=match.id
+        )
+
+        player = UserTournament.objects.get(
+            user_id=user_2.id, tournament_id=tournament.id
+        )
+
+        self.assertEqual(player.round_number, round_number + 1)
