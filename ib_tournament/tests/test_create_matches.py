4e24255b02c55e4115fa8a19c450c7002b3d7bf8
navya <navya.d@ibhubs.co>
2019-02-02 12:23:56 +0530

    test_create_matches: compile error

---
+++ ib_tournament/tests/test_create_matches.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+
+
+def get_next_day_datetime_str():
+    from ib_common.date_time_utils.get_current_local_date_time import \
+        get_current_local_date_time
+    from ib_common.date_time_utils.convert_datetime_to_local_string import \
+        convert_datetime_to_local_string
+    from ib_tournament.constants.general import DEFAULT_DATE_TIME_FORMAT
+    from datetime import timedelta
+
+    next_day_datetime = get_current_local_date_time() + timedelta(days=1)
+    return convert_datetime_to_local_string(
+        next_day_datetime, DEFAULT_DATE_TIME_FORMAT)
+
+
+class TestCreateMatches(TestCase):
+
+    @staticmethod
+    def create_tournament(tournament_details):
+        from ib_tournament.models import Tournament
+        from ib_common.date_time_utils.convert_string_to_local_date_time \
+            import convert_string_to_local_date_time
+        from ib_tournament.constants.general import DEFAULT_DATE_TIME_FORMAT
+
+        start_datetime = convert_string_to_local_date_time(
+            tournament_details['start_datetime'], DEFAULT_DATE_TIME_FORMAT)
+        tournament = Tournament.objects.create(
+            total_rounds=tournament_details['total_rounds'],
+            start_datetime=start_datetime, name=tournament_details['name'])
+        return tournament.id
+
+    def test_create_matches(self):
+        from ib_tournament.models import TournamentMatch
+
+        tournament_details = {
+            'total_rounds': 2,
+            'start_datetime': get_next_day_datetime_str(),
+            'name': 'Tournament 1'
+        }
+        tournament_id = self.create_tournament(tournament_details)
+
+        pre_tournament_matches_count = TournamentMatch.objects.count()
+        TournamentMatch.create_matches(tournament_id)
+        post_tournament_matches_count = TournamentMatch.objects.count()
+        self.assertEqual(
+            post_tournament_matches_count - pre_tournament_matches_count, 3)
