cac826cb0ca63e9505ec2a74528a25bad6f5a933
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-02-02 12:17:57 +0530

    Implemented Dependent Task, Don't know how to implement TDD

---
+++ tournament/models/match.py
@@ -0,0 +1,13 @@
+from django.db import models
+from tournament.models import User, KoTournament
+
+
+class Match(models.Model):
+    MATCH_ID_LENGTH = 20
+    STATUS_LENGTH = 20
+
+    match_id = models.CharField(max_length=MATCH_ID_LENGTH)
+    user = models.ForeignKey(User)
+    tournament = models.ForeignKey(KoTournament)
+    score = models.IntegerField(default=0)
+    status = models.CharField(max_length=STATUS_LENGTH)
