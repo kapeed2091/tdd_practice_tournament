3f12bd2ed0f8e437a053296fb8ec024fba04ddb5
nandeep-tech <nandeep@ibtspl.com>
2019-02-02 20:18:52 +0530

    test_create_round_matches green state

--- tournament/models/round_match.py
+++ tournament/models/round_match.py
@@ -7,4 +7,25 @@ class RoundMatch(models.Model):
 
     @classmethod
     def create_round_matches(cls, tournament_id):
-        pass
+        from .tournament import Tournament
+
+        tournament = \
+            Tournament.get_tournament_by_id(tournament_id=tournament_id)
+        no_of_rounds = tournament.no_of_rounds
+
+        no_of_participants = \
+            Tournament.calculate_no_participants(no_of_rounds=no_of_rounds)
+
+        for round_no in range(1, no_of_rounds + 1):
+            cls.create_matches(round_no=round_no,
+                               no_of_participants=no_of_participants,
+                               tournament_id=tournament_id)
+
+    @classmethod
+    def create_matches(cls, round_no, no_of_participants, tournament_id):
+        no_participants_in_round = no_of_participants / 2 ** round_no
+        no_of_matches_in_round = no_participants_in_round/2
+
+        for index in range(no_of_matches_in_round):
+            cls.objects.create(round_no=round_no, tournament_id=tournament_id)
+
