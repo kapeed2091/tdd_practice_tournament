1087dd6212fb6425c11334502537681f5fad61c2
Ratan Rithwik <rithwik@shirley.local>
2019-02-02 10:24:06 +0530

    green state: create match successful case

--- tournament/models/match.py
+++ tournament/models/match.py
@@ -7,4 +7,7 @@ class Match(models.Model):
 
     @classmethod
     def create_match(cls, tournament_id, round_number):
-        pass
+        cls.objects.create(
+            tournament_id=tournament_id,
+            round_number=round_number
+        )
