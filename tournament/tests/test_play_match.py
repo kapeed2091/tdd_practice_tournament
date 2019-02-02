1c8e90fb7e311a3433a341d2a815ae19b6ff247b
nandeep-tech <nandeep@ibtspl.com>
2019-02-02 16:22:47 +0530

    applied G5(Duplication)

--- tournament/tests/test_play_match.py
+++ tournament/tests/test_play_match.py
@@ -7,20 +7,10 @@ class TestPlayMatch(TestCase):
     match = None
 
     def test_play_match(self):
-        self._populate_user()
-        from ib_common.date_time_utils.get_current_local_date_time \
-            import get_current_local_date_time
-        from datetime import timedelta
-        from tournament.models import Tournament
-
-        curr_datetime = get_current_local_date_time()
         from tdd_practice.constants.general import TournamentStatus
-        self.tournament = Tournament.objects.create(
-            name="Knock Out",
-            no_of_rounds=3,
-            start_datetime=curr_datetime + timedelta(days=1),
-            status=TournamentStatus.IN_PROGRESS.value)
 
+        self._populate_user()
+        self._create_tournament(status=TournamentStatus.IN_PROGRESS.value)
         self._create_match()
         from tournament.models.match import Match
         Match.play_match(match_id=self.match.id, user_id=self.user.id)
@@ -29,20 +19,10 @@ class TestPlayMatch(TestCase):
         self.assertEqual(self.match.status, "IN_PROGRESS")
 
     def test_play_match_when_tournament_not_started(self):
-        self._populate_user()
-        from ib_common.date_time_utils.get_current_local_date_time \
-            import get_current_local_date_time
-        from datetime import timedelta
-        from tournament.models import Tournament
-
-        curr_datetime = get_current_local_date_time()
         from tdd_practice.constants.general import TournamentStatus
-        self.tournament = Tournament.objects.create(
-            name="Knock Out",
-            no_of_rounds=3,
-            start_datetime=curr_datetime + timedelta(days=1),
-            status=TournamentStatus.FULL_YET_TO_START.value)
 
+        self._populate_user()
+        self._create_tournament(status=TournamentStatus.FULL_YET_TO_START.value)
         self._create_match()
 
         with self.assertRaisesMessage(
@@ -62,3 +42,16 @@ class TestPlayMatch(TestCase):
     def _get_match(self):
         from tournament.models.match import Match
         return Match.objects.get(id=self.match.id)
+
+    def _create_tournament(self, status):
+        from ib_common.date_time_utils.get_current_local_date_time \
+            import get_current_local_date_time
+        from datetime import timedelta
+        from tournament.models import Tournament
+
+        curr_datetime = get_current_local_date_time()
+        self.tournament = Tournament.objects.create(
+            name="Knock Out",
+            no_of_rounds=3,
+            start_datetime=curr_datetime + timedelta(days=1),
+            status=status)
