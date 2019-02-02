38029c6bc9668b7786d56b522e676118b3f0c9b8
navya <navya.d@ibhubs.co>
2019-02-02 12:27:42 +0530

    test_create_matches: red state

--- ib_tournament/models/__init__.py
+++ ib_tournament/models/__init__.py
@@ -1,9 +1,11 @@
 from .player import Player
 from .tournament import Tournament
 from .tournament_player import TournamentPlayer
+from .tournament_match import TournamentMatch
 
 __all__ = [
     'Player',
     'Tournament',
-    'TournamentPlayer'
+    'TournamentPlayer',
+    'TournamentMatch'
 ]
---
+++ ib_tournament/models/tournament_match.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class TournamentMatch(models.Model):
+    pass
+
+    @classmethod
+    def create_matches(cls, tournament_id):
+        pass
