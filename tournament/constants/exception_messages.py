cc84206bd744f79d0527bd3564c9a0158af7a5ec
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-02-02 17:09:37 +0530

    TestCase: User Does Not Belong To The Match, State: Refactor(refactoring exception messages to Constants)

--- tournament/constants/exception_messages.py
+++ tournament/constants/exception_messages.py
@@ -1,2 +1,7 @@
 MATCH_CAN_BE_PLAYED_ONLY_AFTER_THE_TOURNAMENT_HAS_STARTED = \
     'Match can be played only after the tournament has started'
+USER_DOES_NOT_EXIST_WITH_THE_GIVEN_USER_ID = \
+    'User does not exist with the given user id'
+USER_DOES_NOT_BELONG_TO_THE_MATCH = 'User does not belong to the match'
+MATCH_DOES_NOT_EXIST_WITH_THE_GIVEN_MATCH_ID = \
+    'Match does not exist with the given match id'
--- tournament/models/match.py
+++ tournament/models/match.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django_swagger_utils.drf_server.exceptions import Forbidden, NotFound
 
-from tournament.constants.exception_messages import MATCH_CAN_BE_PLAYED_ONLY_AFTER_THE_TOURNAMENT_HAS_STARTED
+from tournament.constants.exception_messages import MATCH_CAN_BE_PLAYED_ONLY_AFTER_THE_TOURNAMENT_HAS_STARTED, \
+    USER_DOES_NOT_EXIST_WITH_THE_GIVEN_USER_ID, USER_DOES_NOT_BELONG_TO_THE_MATCH, \
+    MATCH_DOES_NOT_EXIST_WITH_THE_GIVEN_MATCH_ID
 from tournament.constants.general import MatchStatus
 from tournament.models import User, KoTournament
 
@@ -35,7 +37,7 @@ class Match(models.Model):
         try:
             return User.get_user(user_id)
         except User.DoesNotExist:
-            raise NotFound('User does not exist with the given user id')
+            raise NotFound(USER_DOES_NOT_EXIST_WITH_THE_GIVEN_USER_ID)
 
     @staticmethod
     def _validate_tournament(tournament):
@@ -47,12 +49,12 @@ class Match(models.Model):
         try:
             return cls.objects.get(user=user, match_id=match_id)
         except cls.DoesNotExist:
-            raise Forbidden('User does not belong to the match')
+            raise Forbidden(USER_DOES_NOT_BELONG_TO_THE_MATCH)
 
     @classmethod
     def _validate_match(cls, match_id):
         if cls._does_not_match_exists(match_id):
-            raise NotFound('Match does not exist with the given match id')
+            raise NotFound(MATCH_DOES_NOT_EXIST_WITH_THE_GIVEN_MATCH_ID)
 
     @classmethod
     def _does_not_match_exists(cls, match_id):
