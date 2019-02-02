1d653bc4a22131675e5c6e35c32cc6404974fc09
Ratan Rithwik <rithwik@shirley.local>
2019-02-02 11:04:39 +0530

    green state: create match invalid round_number and fix other test cases

--- tournament/models/match.py
+++ tournament/models/match.py
@@ -7,6 +7,9 @@ class Match(models.Model):
 
     @classmethod
     def create_match(cls, tournament_id, round_number):
+        from .tournament import Tournament
+        Tournament.validate_tournament_id(tournament_id=tournament_id)
+
         cls.objects.create(
             tournament_id=tournament_id,
             round_number=round_number
