c299427403213fd88a70835c9dd56a1412bea579
Ratan Rithwik <rithwik@shirley.local>
2019-02-02 12:29:57 +0530

    green state: create UserMatch invalid match id

--- tournament/models/user_match.py
+++ tournament/models/user_match.py
@@ -7,6 +7,13 @@ class UserMatch(models.Model):
 
     @classmethod
     def create_user_match(cls, user_id, match_id):
+        from .match import Match
+        match_exists = Match.objects.filter(id=match_id).exists()
+
+        if not match_exists:
+            from ..exceptions.custom_exceptions import InvalidMatchId
+            raise InvalidMatchId
+
         cls.objects.create(
             user_id=user_id,
             match_id=match_id
