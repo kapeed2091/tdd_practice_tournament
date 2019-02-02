df6761cb8c36d8f6d782a4da3de098d7604b77fc
nandeep-tech <nandeep@ibtspl.com>
2019-02-02 12:40:11 +0530

    test_play_match_when_tournament_not_started red state

--- tournament/tests/test_play_match.py
+++ tournament/tests/test_play_match.py
@@ -15,6 +15,28 @@ class TestPlayMatch(TestCase):
         self.match = self._get_match()
         self.assertEqual(self.match.status, "IN_PROGRESS")
 
+    def test_play_match_when_tournament_not_started(self):
+        from datetime import timedelta
+        from ib_common.date_time_utils.get_current_local_date_time \
+            import get_current_local_date_time
+        from tournament.models import Tournament
+        from tdd_practice.constants.general import TournamentStatus
+
+        self._populate_user()
+        self._create_match()
+
+        curr_datetime = get_current_local_date_time()
+        Tournament.objects.create(
+            name="Knock Out",
+            no_of_rounds=3,
+            start_datetime=curr_datetime + timedelta(days=1),
+            status=TournamentStatus.FULL_YET_TO_START.value)
+
+        with self.assertRaisesMessage(
+                Exception, "Can not play match until tournament is started"):
+            from tournament.models.match import Match
+            Match.play_match(match_id=self.match.id, user_id=self.user.id)
+
     def _populate_user(self):
         from tournament.models.user import User
         self.user = User.objects.create(username=self.username)
