c7891d1295e090e16f0833a3ec2b8cb5baf32e1f
Ratan Rithwik <rithwik@shirley.local>
2019-02-02 13:05:43 +0530

    green state: user not in tournament

--- tournaments/models/user_match.py
+++ tournaments/models/user_match.py
@@ -13,6 +13,21 @@ class UserMatch(models.Model):
         from .match import Match
         Match.validate_match_id(match_id=match_id)
 
+        match = Match.objects.get(id=match_id)
+        tournament_id = match.tournament_id
+
+        from .user_tournament import UserTournament
+        is_user_in_tournament = UserTournament.objects.filter(
+            user_id=user_id,
+            tournament_id=tournament_id
+        ).exists()
+
+        if not is_user_in_tournament:
+            from tournaments.exceptions.custom_exceptions import \
+                UserNotInTournamnet
+
+            raise UserNotInTournamnet
+
         cls.objects.create(
             user_id=user_id,
             match_id=match_id
