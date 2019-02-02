86e473e42b1f34414d7451c2b4757b50be7b6c6d
nandeep-tech <nandeep@ibtspl.com>
2019-02-02 11:47:33 +0530

    test_play_match compilation error

--- tournament/models/__init__.py
+++ tournament/models/__init__.py
@@ -1,7 +1,6 @@
 from .tournament import Tournament
 from .user import User
 from .tournament_user import TournamentUser
-from .tournament_match import TournamentMatch
 
-__all__ = ['Tournament', 'User', 'TournamentUser', 'TournamentMatch']
+__all__ = ['Tournament', 'User', 'TournamentUser']
 
---
+++ tournament/tests/test_play_match.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+
+class TestPlayMatch(TestCase):
+    username = "user1"
+    user = None
+    match = None
+
+    def test_play_match(self):
+        self._populate_user()
+        self._create_match()
+        Match.play_match(match_id=self.match.id, user_id=self.user.id)
+
+        self.match = self._get_match()
+        self.assertEqual(self.match.status, "IN_PROGRESS")
+
+    def _populate_user(self):
+        from tournament.models.user import User
+        self.user = User.objects.create(username=self.username)
+
+    def _create_match(self):
+        self.match = Match.objects.create(user_id=self.user.id)
+
+    def _get_match(self):
+        return Match.objects.get(id=self.match.id)
