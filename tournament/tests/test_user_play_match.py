28ab95dbdb7440f78bea7a666186c2cbacfcf024
Teja Sriram <teja@proyuga.tech>
2019-02-03 13:20:58 +0530

    user play match - user must belong to that match - red state

--- tournament/tests/test_user_play_match.py
+++ tournament/tests/test_user_play_match.py
@@ -209,3 +209,40 @@ class TestUserPlayMatch(TestCase):
             TournamentMatch.user_play_match(
                 user_id=user_id_1, tournament_id=tournament_id,
                 match_id=match_id)
+
+    def testcase_user_should_be_one_of_player_in_match(self):
+        from tournament.models import TournamentMatch, KOTournament, \
+            UserProfile, TournamentUser
+        from ib_common.date_time_utils.get_current_local_date_time import \
+            get_current_local_date_time
+        from datetime import timedelta
+        from tournament.constants import TournamentStatus
+
+        user_id_1 = 'user_1'
+        user_id_2 = 'user_2'
+        user_id_3 = 'user_3'
+        match_id = 'match_1'
+        tournament_id = 'tournament_1'
+        tournament_name = 'city_tournament_1'
+        number_of_rounds = 2
+        start_datetime = get_current_local_date_time() - timedelta(minutes=10)
+
+        UserProfile.objects.create(user_id=user_id_3)
+
+        KOTournament.objects.create(
+            t_id=tournament_id, name=tournament_name,
+            number_of_rounds=number_of_rounds, start_datetime=start_datetime,
+            status=TournamentStatus.IN_PROGRESS.value)
+
+        TournamentUser.objects.create(user_id=user_id_3, t_id=tournament_id)
+
+        TournamentMatch.objects.create(
+            t_id=tournament_id, player_one=user_id_1, player_two=user_id_2,
+            match_id=match_id)
+
+        with self.assertRaisesMessage(
+                Exception,
+                expected_message='User doesnot belong to this match'):
+            TournamentMatch.user_play_match(
+                user_id=user_id_3, tournament_id=tournament_id,
+                match_id=match_id)
