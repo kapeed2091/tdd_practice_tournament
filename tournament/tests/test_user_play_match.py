6e1fedf245991fa6eb8f6c68cce21e3d4b18d928
Teja Sriram <teja@proyuga.tech>
2019-02-03 11:53:03 +0530

    user play match - current datetime is greater than tournament datetime - red state

--- tournament/tests/test_user_play_match.py
+++ tournament/tests/test_user_play_match.py
@@ -54,3 +54,32 @@ class TestUserPlayMatch(TestCase):
             TournamentMatch.user_play_match(
                 user_id=user_id_1, tournament_id=tournament_id,
                 match_id=match_id)
+
+    def testcase_user_play_match_after_tournament_start_datetime(self):
+        from tournament.models import TournamentMatch, KOTournament
+        from ib_common.date_time_utils.get_current_local_date_time import \
+            get_current_local_date_time
+        from datetime import timedelta
+
+        user_id_1 = 'user_1'
+        user_id_2 = 'user_2'
+        match_id = 'match_1'
+        tournament_id = 'tournament_1'
+        tournament_name = 'city_tournament_1'
+        number_of_rounds = 2
+        start_datetime = get_current_local_date_time() + timedelta(minutes=10)
+
+        KOTournament.objects.create(
+            t_id=tournament_id, name=tournament_name,
+            number_of_rounds=number_of_rounds, start_datetime=start_datetime)
+
+        TournamentMatch.objects.create(
+            t_id=tournament_id, player_one=user_id_1, player_two=user_id_2,
+            match_id=match_id)
+
+        with self.assertRaisesMessage(
+                Exception,
+                expected_message='Match cannot be played before start datetime'):
+            TournamentMatch.user_play_match(
+                user_id=user_id_1, tournament_id=tournament_id,
+                match_id=match_id)
