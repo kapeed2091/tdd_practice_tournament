c11b7c63ccd1534e69f944c600665b923e53002d
Teja Sriram <teja@proyuga.tech>
2019-02-04 10:32:38 +0530

    user submit score - compile error

---
+++ tournament/tests/test_user_submit_score.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+
+class TestUserSubmitScore(TestCase):
+
+    def testcase_user_submit_score(self):
+        from tournament.models import TournamentMatch
+        user_id_1 = 'user_1'
+        user_id_2 = 'user_2'
+        match_id = 'match_1'
+        tournament_id = 'tournament_1'
+        score = 10
+
+        TournamentMatch.objects.create(
+            t_id=tournament_id, player_one=user_id_1, player_two=user_id_2,
+            match_id=match_id)
+
+        old_state = TournamentMatch.objects.get(
+            player_one=user_id_1, match_id=match_id)
+
+        TournamentMatch.user_submit_score(user_id_1, match_id, score)
+
+        new_state = TournamentMatch.objects.get(
+            player_one=user_id_1, match_id=match_id)
+
+        self.assertEquals(
+            new_state.player_one_score - old_state.player_one_score, score)
