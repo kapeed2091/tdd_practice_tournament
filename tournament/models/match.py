12066747d2cec8a8825fda19f8d15cc9bd0b7a26
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-02-04 09:18:31 +0530

    Adding new fields to match Model

--- tournament/models/match.py
+++ tournament/models/match.py
@@ -11,12 +11,15 @@ from tournament.models import User, KoTournament
 class Match(models.Model):
     MATCH_ID_LENGTH = 20
     STATUS_LENGTH = 20
+    USER_STATUS_LENGTH = 20
 
     match_id = models.CharField(max_length=MATCH_ID_LENGTH)
     user = models.ForeignKey(User)
     tournament = models.ForeignKey(KoTournament)
     score = models.IntegerField(default=0)
+    round = models.IntegerField(default=0)
     status = models.CharField(max_length=STATUS_LENGTH)
+    user_status = models.CharField(max_length=USER_STATUS_LENGTH)
 
     @classmethod
     def submit_score(cls, user_id, match_id, score):
