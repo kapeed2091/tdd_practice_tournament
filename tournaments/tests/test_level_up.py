a559758d3d3254a704585730b486f40fe1177ed8
Ratan Rithwik <rithwik@shirley.local>
2019-02-04 09:59:22 +0530

    red state: match id does not exist

--- tournaments/tests/test_level_up.py
+++ tournaments/tests/test_level_up.py
@@ -114,3 +114,38 @@ class TestLevelUp(TestUtils):
             UserTournament.level_up(
                 user_id=user_3.id, match_id=match.id
             )
+
+    def test_match_id_does_not_exist(self):
+        user_1 = self.create_user()
+        user_2 = self.create_user(name="John-2")
+
+        tournament = self.create_tournament(user_id=user_1.id)
+
+        self.create_user_tournament(
+            user_id=user_1.id, tournament_id=tournament.id
+        )
+        self.create_user_tournament(
+            user_id=user_2.id, tournament_id=tournament.id
+        )
+
+        round_number = 2
+        match = self.create_match(
+            tournament_id=tournament.id, round_number=round_number
+        )
+
+        self.create_user_match(
+            user_id=user_1.id, match_id=match.id, score=100
+        )
+
+        self.create_user_match(
+            user_id=user_2.id, match_id=match.id, score=200
+        )
+
+        match_id = 3
+        from tournaments.models import UserTournament
+        from tournaments.exceptions.custom_exceptions import \
+            InvalidMatchId
+        with self.assertRaises(InvalidMatchId):
+            UserTournament.level_up(
+                user_id=user_2.id, match_id=match_id
+            )
