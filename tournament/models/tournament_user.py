998bf7c683fe373caac167c40f63af851b20a14d
Teja Sriram <teja@proyuga.tech>
2019-02-06 16:13:47 +0530

    get user current round number - user should be subscribed to tournament - green state

--- tournament/models/tournament_user.py
+++ tournament/models/tournament_user.py
@@ -30,6 +30,7 @@ class TournamentUser(models.Model):
 
     @classmethod
     def get_user_current_round(cls, t_id, user_id):
+        cls.validate_user_subscription(tournament_id=t_id, user_id=user_id)
         tournament_user_obj = cls.objects.get(user_id=user_id, t_id=t_id)
         return tournament_user_obj.current_round_number
 
