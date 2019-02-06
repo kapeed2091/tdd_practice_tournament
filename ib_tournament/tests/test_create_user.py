3682eed9f5fc1fa7aba744d10288ddd87ab83ca6
navya <navya.d@ibhubs.co>
2019-02-06 12:56:10 +0530

    test_user_fields_updation:red state

--- ib_tournament/tests/test_create_user.py
+++ ib_tournament/tests/test_create_user.py
@@ -19,3 +19,18 @@ class TestCreateUser(TestCase):
         from django_swagger_utils.drf_server.exceptions import BadRequest
         with self.assertRaisesMessage(BadRequest, "Username already Exists"):
             Player.create_player(username_2)
+
+    def test_user_fields_updation(self):
+        user_details = {
+            'username': 'user1',
+            'name': 'User 1',
+            'age': 22,
+            'gender': 'MALE'
+        }
+        from ib_tournament.models import Player
+        Player.create_player(user_details)
+        player = Player.objects.get(username=user_details['username'])
+        self.assertEqual(player.username, user_details['username'])
+        self.assertEqual(player.name, user_details['name'])
+        self.assertEqual(player.age, user_details['age'])
+        self.assertEqual(player.gender, user_details['gender'])
