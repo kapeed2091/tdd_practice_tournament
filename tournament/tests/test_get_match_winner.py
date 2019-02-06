2bf0eac5b7280a37adda22c53d7b5da83ea40806
nandeep-tech <nandeep@ibtspl.com>
2019-02-06 11:36:05 +0530

    test_get_match_winner_for_tie_match red state

--- tournament/tests/test_get_match_winner.py
+++ tournament/tests/test_get_match_winner.py
@@ -24,6 +24,26 @@ class TestGetMatchWinner(TestCase):
 
         self.assertEqual(expected_winner_id, winner_id)
 
+    def test_get_match_winner_for_tie_match(self):
+        from tdd_practice.constants.general import TournamentStatus
+        self.users = self._populate_users()
+        self._create_tournament(status=TournamentStatus.IN_PROGRESS.value)
+        self._create_user_matches()
+
+        user_id_wise_score = {}
+        for index, user in enumerate(self.users):
+            score = 10
+            user_id_wise_score[user.id] = score
+
+        self._submit_users_score(user_id_wise_score)
+
+        from tournament.models import RoundMatch
+        winner_id = RoundMatch.get_match_winner(match_id=self.match.id)
+
+        expected_winner_id = self.users[0].id
+
+        self.assertEqual(expected_winner_id, winner_id)
+
     def _create_user_matches(self):
         from tournament.models import RoundMatch
         self.match = RoundMatch.objects.create(
@@ -60,10 +80,14 @@ class TestGetMatchWinner(TestCase):
     def _submit_users_score(self, user_id_wise_score):
         from tournament.models import Match
         from tdd_practice.constants.general import UserMatchStatus
+        from ib_common.date_time_utils.get_current_local_date_time \
+            import get_current_local_date_time
 
         for user_id, score in user_id_wise_score.items():
+            curr_datetime = get_current_local_date_time()
             user_match = Match.objects.get(
                 round_match_id=self.match.id, user_id=user_id)
             user_match.score = score
             user_match.status = UserMatchStatus.COMPLETED.value
+            user_match.score_submission_datetime = curr_datetime
             user_match.save()
