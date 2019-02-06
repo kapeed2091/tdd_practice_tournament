5ee104e53cebae2e7cc35911bc0e387197e0ddd7
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-02-06 10:08:30 +0530

    TestCase: Get Match To Progress, State: Compile error

--- tournament/tests/test_progress_user.py
+++ tournament/tests/test_progress_user.py
@@ -16,7 +16,7 @@ class TestProgressUser(TestCase):
     match2_id = 'Match2'
     match3_id = 'Match3'
 
-    def setUp(self):
+    def setup_common(self):
         from tournament.models import User, KoTournament, Match
 
         now = get_current_datetime()
@@ -87,6 +87,7 @@ class TestProgressUser(TestCase):
     def test_progress_winner_to_next_round(self):
         from tournament.models import Match, User
 
+        self.setup_common()
         match2_before = Match.objects.get(match_id=self.match2_id)
         self.assertEqual(match2_before.user, None)
         Match.progress_match_winner_to_next_round(match_id=self.match1_id)
@@ -98,5 +99,51 @@ class TestProgressUser(TestCase):
     def test_there_is_no_next_round_to_progress(self):
         from tournament.models import Match
 
+        self.setup_common()
         with self.assertRaisesMessage(BadRequest, 'There are no further rounds in this tournament'):
             Match.progress_match_winner_to_next_round(match_id=self.match3_id)
+
+    def test_get_match_to_progress(self):
+        from tournament.models import Match, KoTournament
+
+        self.setup_get_match_to_progress()
+
+        tournament1 = KoTournament.objects.get(
+            name='Tournament1'
+        )
+        match = Match.get_match_to_assign_v2(round=2, tournament=tournament1)
+        self.assertEqual(match.user, None)
+        self.assertEqual(match.round, 2)
+        self.assertEqual(match.tournament, tournament1)
+
+    def setup_get_match_to_progress(self):
+        from tournament.models import Match, KoTournament
+
+        now = get_current_datetime()
+        tournament_name = 'Tournament'
+        for i in range(1, 3):
+            tournament = KoTournament.objects.create(
+                created_user_id=self.user_id,
+                name=tournament_name + str(i),
+                no_of_rounds=3,
+                start_datetime=now - datetime.timedelta(days=1),
+                status=TournamentStatus.IN_PROGRESS.value
+            )
+
+            match_id = 'Match'
+            for j in range(1, 5):
+                Match.objects.create(
+                    match_id=match_id + str(i) + str(j),
+                    tournament=tournament,
+                    round=2,
+                    status=MatchStatus.YET_TO_START.value,
+                    user_status=MatchUserStatus.NOT_DECIDED_YET.value
+                )
+            for j in range(1, 3):
+                Match.objects.create(
+                    match_id=match_id + str(i) + str(j),
+                    tournament=tournament,
+                    round=3,
+                    status=MatchStatus.YET_TO_START.value,
+                    user_status=MatchUserStatus.NOT_DECIDED_YET.value
+                )
