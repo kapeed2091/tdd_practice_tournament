ef676b2020ddaa2d1b101f02ba629a191ce1baf9
Ratan Rithwik <rithwik@shirley.local>
2019-02-06 14:50:46 +0530

    Compilation error: inadequate_number_of_matches

--- tournaments/tests/test_assign_players_to_match.py
+++ tournaments/tests/test_assign_players_to_match.py
@@ -71,3 +71,35 @@ class TestAssignPlayersToMatch(TestUtils):
             UserMatch.assign_players(
                 tournament_id=tournament.id, round_number=round_number
             )
+
+    def test_inadequate_number_of_matches(self):
+        user = self.create_user()
+
+        users = []
+        for each in range(4):
+            name = "John-" + str(each + 2)
+            users.append(self.create_user(name=name))
+
+        tournament = self.create_tournament(user_id=user.id)
+
+        self.create_tournament_matches(
+            tournament_id=tournament.id,
+            total_rounds=tournament.total_rounds - 2
+        )
+
+        round_number = 3
+
+        for each_user in users:
+            self.create_user_tournament(
+                user_id=each_user.id,
+                tournament_id=tournament.id,
+                round_number=round_number
+            )
+
+        from tournaments.models import UserMatch
+        from tournaments.exceptions.custom_exceptions import \
+            InadequateNumberOfMatches
+        with self.assertRaises(InadequateNumberOfMatches):
+            UserMatch.assign_players(
+                tournament_id=tournament.id, round_number=round_number
+            )
