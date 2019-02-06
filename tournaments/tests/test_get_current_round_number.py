d699bc284b52ca52fb62d64093ba5bf7bdc46045
Ratan Rithwik <rithwik@shirley.local>
2019-02-06 16:25:58 +0530

    red state: user not in tournament

--- tournaments/tests/test_get_current_round_number.py
+++ tournaments/tests/test_get_current_round_number.py
@@ -17,3 +17,22 @@ class TestGetCurrentRoundNumber(TestUtils):
         )
 
         self.assertEqual(user_tournament.round_number, round_number)
+
+    def test_user_not_in_tournament(self):
+        user = self.create_user()
+        user_2 = self.create_user("John-2")
+        tournament = self.create_tournament(user_id=user.id)
+
+        self.create_user_tournament(
+            user_id=user.id, tournament_id=tournament.id
+        )
+
+        from tournaments.models import UserTournament
+
+        user_id = user_2.id
+        from tournaments.exceptions.custom_exceptions import \
+            UserNotInTournament
+        with self.assertRaises(UserNotInTournament):
+            UserTournament.get_current_round_number(
+                user_id=user_id, tournament_id=tournament.id
+            )
