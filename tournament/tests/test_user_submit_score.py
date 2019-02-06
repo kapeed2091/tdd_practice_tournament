8f3e1aa44710bed76ea2515ef76d3549141b6b5b
Teja Sriram <teja@proyuga.tech>
2019-02-06 11:44:29 +0530

    user submit score along with time - compile error

--- tournament/tests/test_user_submit_score.py
+++ tournament/tests/test_user_submit_score.py
@@ -119,3 +119,42 @@ class TestUserSubmitScore(TestCase):
         self.assertEquals(
             player_two_new_state.player_two_score -
             player_two_old_state.player_two_score, user_2_score)
+
+    def testcase_user_submit_score_should_be_recorded_along_with_time(self):
+        from tournament.models import TournamentMatch, UserProfile
+        from datetime import timedelta
+        from ib_common.date_time_utils.get_current_local_date_time \
+            import get_current_local_date_time
+
+        user_id_1 = 'user_1'
+        user_id_2 = 'user_2'
+        match_id = 'match_1'
+        tournament_id = 'tournament_1'
+        user_1_score = 10
+        user_2_score = 20
+
+        user_1_time = get_current_local_date_time()
+        user_2_time = get_current_local_date_time() + timedelta(minutes=5)
+
+        UserProfile.objects.create(user_id=user_id_1)
+        UserProfile.objects.create(user_id=user_id_2)
+
+        TournamentMatch.objects.create(
+            t_id=tournament_id, player_one=user_id_1, player_two=user_id_2,
+            match_id=match_id)
+
+        TournamentMatch.user_submit_score_with_time(
+            user_id=user_id_1, match_id=match_id, score=user_1_score,
+            submit_time=user_1_time)
+        TournamentMatch.user_submit_score_with_time(
+            user_id=user_id_2, match_id=match_id, score=user_2_score,
+            submit_time=user_2_time)
+
+        player_one_state = TournamentMatch.objects.get(
+            player_one=user_id_1, match_id=match_id)
+
+        player_two_state = TournamentMatch.objects.get(
+            player_two=user_id_2, match_id=match_id)
+
+        self.assertEquals(player_one_state.player_one_submit_time, user_1_time)
+        self.assertEquals(player_two_state.player_two_submit_time, user_2_time)
