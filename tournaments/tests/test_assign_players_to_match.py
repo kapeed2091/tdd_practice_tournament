24f13937ca08d40ed3403e49ec23f10994f3a424
Ratan Rithwik <rithwik@shirley.local>
2019-02-06 12:32:56 +0530

    Compilation error: assign_players successful

---
+++ tournaments/tests/test_assign_players_to_match.py
@@ -0,0 +1,42 @@
+from .test_utils import TestUtils
+
+
+class TestAssignPlayersToMatch(TestUtils):
+
+    def test_assign_players_successful(self):
+        user = self.create_user()
+
+        users = []
+        for each in range(4):
+            name = "John-" + str(each + 2)
+            users.append(self.create_user(name=name))
+
+        tournament = self.create_tournament(user_id=user.id)
+
+        self.create_tournament_matches(
+            tournament_id=tournament.id, total_rounds=tournament.total_rounds
+        )
+
+        round_number = 3
+
+        for each_user in users:
+            self.create_user_tournament(
+                user_id=each_user.id,
+                tournament_id=tournament.id,
+                round_number=round_number
+            )
+
+        from tournaments.models import UserMatch
+
+        initial_count_of_user_matches = UserMatch.objects.all().count()
+        UserMatch.assign_players(
+            tournament_id=tournament.id, round_number=round_number
+        )
+        final_count_of_user_matches = UserMatch.objects.all().count()
+
+        number_of_people_in_round = 2 ** (
+                tournament.total_rounds - round_number + 1)
+
+        self.assertEqual(0, initial_count_of_user_matches)
+        self.assertEqual(number_of_people_in_round,
+                         final_count_of_user_matches)
