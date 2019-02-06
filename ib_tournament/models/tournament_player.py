22baa28b43f301c6705f516acaf64bf4fbbe3cdd
navya <navya.d@ibhubs.co>
2019-02-06 11:25:05 +0530

    test_get_current_round_by_user: red state

--- ib_tournament/models/tournament_player.py
+++ ib_tournament/models/tournament_player.py
@@ -4,6 +4,7 @@ from django.db import models
 class TournamentPlayer(models.Model):
     tournament = models.ForeignKey('ib_tournament.Tournament')
     player = models.ForeignKey('ib_tournament.Player')
+    curr_round_no = models.IntegerField(default=1)
 
     class Meta:
         unique_together = ('tournament', 'player')
@@ -35,3 +36,7 @@ class TournamentPlayer(models.Model):
     def get_player_ids_of_tournament(cls, tournament_id):
         return list(cls.objects.filter(tournament_id=tournament_id).values_list(
             'player_id', flat=True))
+
+    @classmethod
+    def get_player_current_round(cls, tournament_id, player_id):
+        return
