3a7e48f660fbebeb68c2e20c8eb1c24afa9bc61b
Teja Sriram <teja@proyuga.tech>
2019-02-07 11:34:40 +0530

    get opponent profile - user should be subscribed to tournament- red state

--- tournament/tests/test_user_get_opponent_user_profile.py
+++ tournament/tests/test_user_get_opponent_user_profile.py
@@ -61,3 +61,43 @@ class TestUserGetOpponentProfile(TestCase):
 
         self.assertEquals(round_1_opponent_profile, user_2_profile)
         self.assertEquals(round_2_opponent_profile, user_3_profile)
+
+    def testcase_check_user_subscription_of_tournament_to_get_opponent_profile(
+            self):
+        from tournament.models import TournamentMatch, UserProfile
+
+        user_id_1 = 'user_1'
+        name_1 = 'John'
+        age_1 = 24
+        gender_1 = 'MALE'
+
+        user_id_2 = 'user_2'
+        name_2 = 'Robin'
+        age_2 = 25
+        gender_2 = 'FEMALE'
+
+        user_id_3 = 'user_3'
+        name_3 = 'Lee'
+        age_3 = 28
+        gender_3 = 'MALE'
+
+        t_id = 'tournament_1'
+        t_round_number = 1
+
+        UserProfile.objects.create(
+            user_id=user_id_1, name=name_1, age=age_1, gender=gender_1)
+        UserProfile.objects.create(
+            user_id=user_id_2, name=name_2, age=age_2, gender=gender_2)
+        UserProfile.objects.create(
+            user_id=user_id_3, name=name_3, age=age_3, gender=gender_3)
+
+        TournamentMatch.objects.create(
+            t_id=t_id, t_round_number=t_round_number, player_one=user_id_1,
+            player_two=user_id_2)
+
+        with self.assertRaisesMessage(
+                Exception,
+                expected_message='User not subscribed to tournament'):
+            TournamentMatch.get_opponent_user_profile(
+                tournament_id=t_id, round_number=t_round_number,
+                user_id=user_id_3)
