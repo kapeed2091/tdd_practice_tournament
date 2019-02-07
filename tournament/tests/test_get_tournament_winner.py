a2bb7fd47ad85284a3cb83e1cc1b8fec3861b58c
nandeep-tech <nandeep@ibtspl.com>
2019-02-07 10:34:27 +0530

    refactoring - level of abstraction(G34)

--- tournament/tests/test_get_tournament_winner.py
+++ tournament/tests/test_get_tournament_winner.py
@@ -16,13 +16,12 @@ class TestGetTournamentWinner(TestCase):
         winner_profile = Tournament.\
             get_tournament_winner_profile(tournament_id=self.tournament.id)
 
-        exp_winner_profile = {
-            "user_id": self.user.id,
-            "name": self.user.name,
-            "gender": self.user.gender,
-            "age": self.user.age
-        }
+        exp_winner_profile = self._get_expected_winner_profile()
         self.assertEqual(exp_winner_profile, winner_profile)
+
+    def _populate_user(self):
+        from tournament.models.user import User
+        self.user = User.objects.create(username=self.username)
 
     def _create_tournament(self, status, winner):
         from ib_common.date_time_utils.get_current_local_date_time \
@@ -37,6 +36,11 @@ class TestGetTournamentWinner(TestCase):
             winner=winner,
             status=status)
 
-    def _populate_user(self):
-        from tournament.models.user import User
-        self.user = User.objects.create(username=self.username)
+    def _get_expected_winner_profile(self):
+        exp_winner_profile = {
+            "user_id": self.user.id,
+            "name": self.user.name,
+            "gender": self.user.gender,
+            "age": self.user.age
+        }
+        return exp_winner_profile
