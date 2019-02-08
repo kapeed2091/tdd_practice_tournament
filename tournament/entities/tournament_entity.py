94f7df4230bb99a5b73b6a35716e33d778de25b2
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-02-08 11:41:32 +0530

    Refactored

--- tournament/entities/tournament_entity.py
+++ tournament/entities/tournament_entity.py
@@ -10,14 +10,22 @@ class Tournament(object):
                           start_datetime=start_datetime)
 
     def join_tournament(self, username):
-        if username in self.usernames:
+        self._validate_join_tournament(username=username)
+        self.usernames.append(username)
+
+    def _validate_join_tournament(self, username):
+        if self._is_username_already_joined(username):
             from tournament.exceptions import JoinTournamentDuplicateUser
             raise JoinTournamentDuplicateUser(
                 'User cannot join a tournament multiple times')
 
-        if len(self.usernames) == 2**self.no_of_rounds:
+        if self._is_tournament_full():
             from tournament.exceptions import \
                 JoinTournamentMaximumCountExceeded
             raise JoinTournamentMaximumCountExceeded('Tournament is full')
 
-        self.usernames.append(username)
+    def _is_username_already_joined(self, username):
+        return username in self.usernames
+
+    def _is_tournament_full(self):
+        return len(self.usernames) == 2**self.no_of_rounds
