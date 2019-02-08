31ce85ab5396cf71b31063cb162eeb3a49db6fb7
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-02-08 10:52:15 +0530

    Added testcase for join tournament: RED state

--- tournament/entities/tournament.py
+++ tournament/entities/tournament.py
@@ -2,8 +2,12 @@ class Tournament(object):
     def __init__(self, no_of_rounds, start_datetime):
         self.no_of_rounds = no_of_rounds
         self.start_datetime = start_datetime
+        self.usernames = []
 
     @staticmethod
     def create_tournament(no_of_rounds, start_datetime):
         return Tournament(no_of_rounds=no_of_rounds,
                           start_datetime=start_datetime)
+
+    def join_tournament(self, username):
+        pass
--- tournament/tests/entities/test_tournament.py
+++ tournament/tests/entities/test_tournament.py
@@ -5,6 +5,7 @@ class TestTournament(TestCase):
     import datetime
     NO_OF_ROUNDS = 4
     START_DATETIME = datetime.datetime.now()
+    USERNAME = 'deepak'
 
     def test_create_tournament(self):
         from tournament.entities import Tournament
@@ -15,3 +16,9 @@ class TestTournament(TestCase):
                           'Number of rounds did not match')
         self.assertEquals(tournament_obj.start_datetime, self.START_DATETIME,
                           'Start Datetime did not match')
+
+    def test_join_tournament(self):
+        from tournament.entities import Tournament
+        tournament_obj = Tournament(no_of_rounds=self.NO_OF_ROUNDS, start_datetime=self.START_DATETIME)
+        tournament_obj.join_tournament(username=self.USERNAME)
+        self.assertIn(self.USERNAME, tournament_obj.usernames, 'User did not subscribe')
