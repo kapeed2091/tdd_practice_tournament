56067547ec7de74bb8f4d99844e5f70c23f2a89b
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-02-09 20:28:59 +0530

    create matches for tournament, each player should contain unique match

--- tournament/tests/usecases/test_create_matches_in_tournament.py
+++ tournament/tests/usecases/test_create_matches_in_tournament.py
@@ -63,3 +63,37 @@ class TestCreateMatchesInTournament(TestCase):
         self.assertEqual(presenter.present_create_matches_in_tournament.return_value,
                          response)
         self.assertTrue(presenter.present_create_matches_in_tournament.called)
+
+    def test_create_matches_player_(self):
+        storage = mock.Mock()
+        presenter = mock.Mock()
+
+        storage.get_tournament_data.return_value = {
+            'no_of_rounds': self.NO_OF_ROUNDS,
+            'start_datetime': self.START_DATETIME,
+            'player_usernames': ['USERNAME_'+str(i+1) for i in range(0, 2**(self.NO_OF_ROUNDS))]
+        }
+        presenter.present_create_tournament.return_value = {}
+
+        from tournament.usecases.interactors import \
+            CreateMatchesInTournamentInteractor
+        create_matches_in_tournament_interactor = \
+            CreateMatchesInTournamentInteractor(
+                storage=storage, presenter=presenter
+            )
+        create_matches_in_tournament_interactor.setup(tournament_id=1)
+        response = create_matches_in_tournament_interactor.execute()
+
+        (create_matches_data,), _ = storage.create_matches.call_args
+
+        tournament_player_usernames = []
+
+        for match in create_matches_data:
+            player_usernames = match['player_usernames']
+            for username in player_usernames:
+                self.assertNotIn(username, tournament_player_usernames, 'Player should have a unique match')
+            tournament_player_usernames.extend(player_usernames)
+
+        self.assertEqual(presenter.present_create_matches_in_tournament.return_value,
+                         response)
+        self.assertTrue(presenter.present_create_matches_in_tournament.called)
