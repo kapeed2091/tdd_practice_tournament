57a8b187a3df7b5b5ab3c314f0beee138dc5d919
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-02-09 15:15:51 +0530

    Added testcase for create tournament: Green state

--- tournament/usecases/create_tournament_usecase.py
+++ tournament/usecases/create_tournament_usecase.py
@@ -1,6 +1,13 @@
 class CreateTournamentUsecase(object):
+    def __init__(self):
+        self.no_of_rounds = 0
+        self.start_datetime = ''
+
     def setup(self, no_of_rounds, start_datetime):
+        self.no_of_rounds = no_of_rounds
+        self.start_datetime = start_datetime
         pass
 
     def execute(self):
-        return {'no_of_rounds': 0, 'start_datetime': ''}
+        return {'no_of_rounds': self.no_of_rounds,
+                'start_datetime': self.start_datetime}
