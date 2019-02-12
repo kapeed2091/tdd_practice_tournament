8b4b16e5be8af8afdc6419bdb1acd508d4a638a3
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-02-12 11:06:46 +0530

    Create tournament testcase - Red state (post step-3)

--- tournament/usecases/create_tournament_interactor.py
+++ tournament/usecases/create_tournament_interactor.py
@@ -1,6 +1,17 @@
 class CreateTournamentInteractor(object):
+    def __init__(self, storage=None, presenter=None):
+        self.storage=storage
+        self.presenter=presenter
+
     def setup(self, no_of_rounds, start_datetime):
-        pass
+        self.no_of_rounds = no_of_rounds
+        self.start_datetime = start_datetime
 
     def execute(self):
-        pass
+        #processLogic
+        tournament_id = self.storage.create_tournament(
+            no_of_rounds=self.no_of_rounds, start_datetime=self.start_datetime)
+        tournament_data = self.storage.get_tournament(tournament_id)
+        #prepareData
+        return self.presenter.present_create_tournament(tournament_data,
+                                                        tournament_id)
