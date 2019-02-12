84325eaa1c8900ba66fa322aa54c845c7339a606
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-02-12 11:56:12 +0530

    Test get tournaments: Green state (post 5th step)

--- tournament/tests/usecases/test_get_tournaments.py
+++ tournament/tests/usecases/test_get_tournaments.py
@@ -12,7 +12,22 @@ class TestGetTournaments(TestCase):
             }
         ]
 
+        from mock import Mock
+        storage = Mock()
+        storage.get_tournaments.return_value = tournaments
+
+        presenter = Mock()
+        presenter.present_get_tournaments.return_value = tournaments
+
         from tournament.usecases import GetTournamentsInteractor
-        usecase = GetTournamentsInteractor()
+        usecase = GetTournamentsInteractor(storage=storage, presenter=presenter)
         usecase_response = usecase.execute()
         self.assertEqual(usecase_response, tournaments)
+
+        self.assertTrue(storage.get_tournaments.called)
+        get_tournament_input, _ = storage.get_tournaments.call_args
+        self.assertEqual(get_tournament_input, (), 'No arguments for get tournaments storage')
+
+        self.assertTrue(presenter.present_get_tournaments)
+        (presenter_get_tournaments_input, ), _ = presenter.present_get_tournaments.call_args
+        self.assertEqual(presenter_get_tournaments_input, tournaments)
