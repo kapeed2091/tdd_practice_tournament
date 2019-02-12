e941a2030b92ff0f72ca18ce673fb676a297165c
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-02-12 11:11:55 +0530

    Create tournament testcase - Green state (post step-4, 5)

--- tournament/tests/usecases/test_create_tournament.py
+++ tournament/tests/usecases/test_create_tournament.py
@@ -12,8 +12,37 @@ class TestCreateTournament(TestCase):
             'no_of_rounds': self.NO_OF_ROUNDS,
             'start_datetime': self.START_DATETIME
         }
+
+        tournament_id = tournament_data['tournament_id']
+
+        from copy import deepcopy
+        tournament_data_without_id = deepcopy(tournament_data)
+        tournament_data_without_id.pop('tournament_id')
+
+        from mock import Mock
+
+        storage = Mock()
+        storage.create_tournament.return_value = tournament_id
+        storage.get_tournament.return_value = tournament_data_without_id
+
+        presenter = Mock()
+        presenter.present_create_tournament.return_value = tournament_data
+
         from tournament.usecases import CreateTournamentInteractor
-        usecase = CreateTournamentInteractor()
+        usecase = CreateTournamentInteractor(
+            storage=storage, presenter=presenter
+        )
         usecase.setup(no_of_rounds=self.NO_OF_ROUNDS, start_datetime=self.START_DATETIME)
         usecase_response = usecase.execute()
         self.assertEqual(usecase_response, tournament_data)
+
+        (no_of_rounds, start_datetime), _ = storage.create_tournament.call_args
+        self.assertEqual(no_of_rounds, self.NO_OF_ROUNDS)
+        self.assertEqual(start_datetime, self.START_DATETIME)
+
+        (get_tournament_tournament_id,), _ = storage.get_tournament.call_args
+        self.assertEqual(get_tournament_tournament_id, tournament_id)
+
+        (presenter_input_tournament_data, presenter_tournament_id), _ = presenter.present_create_tournament.call_args
+        self.assertEqual(presenter_tournament_id, tournament_id)
+        self.assertEqual(presenter_input_tournament_data, tournament_data_without_id)
--- tournament/usecases/create_tournament_interactor.py
+++ tournament/usecases/create_tournament_interactor.py
@@ -10,7 +10,7 @@ class CreateTournamentInteractor(object):
     def execute(self):
         #processLogic
         tournament_id = self.storage.create_tournament(
-            no_of_rounds=self.no_of_rounds, start_datetime=self.start_datetime)
+            self.no_of_rounds, self.start_datetime)
         tournament_data = self.storage.get_tournament(tournament_id)
         #prepareData
         return self.presenter.present_create_tournament(tournament_data,
