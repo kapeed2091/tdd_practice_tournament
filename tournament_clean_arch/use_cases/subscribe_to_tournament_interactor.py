ab3b5ae6ad3fe52a58be861b1eefe750721f9877
rithweek <rithwik@ibhubs.co>
2019-05-14 19:31:19 +0530

    green state: tournament is full

--- tournament_clean_arch/use_cases/subscribe_to_tournament_interactor.py
+++ tournament_clean_arch/use_cases/subscribe_to_tournament_interactor.py
@@ -23,10 +23,34 @@ class SubscribeToTournamentInteractor(object):
                 TournamentHasStarted
             raise TournamentHasStarted
 
+    def validate_if_tournament_is_full(self, tournament_id):
+        tournament_details = self.storage.get_tournament(
+            tournament_id=tournament_id
+        )
+        players_count = self.storage.get_total_subscribers_for_tournament(
+            tournament_id=tournament_id
+        )
+
+        no_of_rounds = tournament_details["no_of_rounds"]
+        max_allowed_players_for_tournament = \
+            self.get_max_allowed_players_for_tournament(
+                no_of_rounds=no_of_rounds)
+
+        if players_count >= max_allowed_players_for_tournament:
+            from tournament_clean_arch.exceptions.custom_exceptions import \
+                TournamentIsFull
+            raise TournamentIsFull
+
+    @staticmethod
+    def get_max_allowed_players_for_tournament(no_of_rounds):
+        return 2 ** no_of_rounds
+
     def execute(self):
         self.validate_tournament_has_started(
             tournament_id=self.tournament_id
         )
+        self.validate_if_tournament_is_full(tournament_id=self.tournament_id)
+
         subscribe_id = self.storage.subscribe_to_tournament(
             user_id=self.user_id,
             tournament_id=self.tournament_id
