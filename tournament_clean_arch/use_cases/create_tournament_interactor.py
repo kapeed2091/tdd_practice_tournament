f5f2012b3a14914bc7800130242eb037e4327218
rithweek <rithwik@ibhubs.co>
2019-05-14 16:42:09 +0530

    green state

--- tournament_clean_arch/use_cases/create_tournament_interactor.py
+++ tournament_clean_arch/use_cases/create_tournament_interactor.py
@@ -9,7 +9,16 @@ class CreateTournamentInteractor(object):
         self.no_of_rounds = no_of_rounds
         self.start_datetime = start_datetime
 
+    @staticmethod
+    def validate_no_of_rounds(no_of_rounds):
+        if no_of_rounds < 3:
+            from tournament_clean_arch.constants.custom_exceptions import \
+                InvalidNumberOfRounds
+            raise InvalidNumberOfRounds
+
     def execute(self):
+        self.validate_no_of_rounds(no_of_rounds=self.no_of_rounds)
+
         tournament_id = self.storage.create_tournament(
             no_of_rounds=self.no_of_rounds,
             start_datetime=self.start_datetime,
