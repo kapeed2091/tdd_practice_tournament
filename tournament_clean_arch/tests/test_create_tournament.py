716ef18e7e963a82b0c426e170921a87646f0f1e
rithweek <rithwik@ibhubs.co>
2019-05-14 16:37:11 +0530

    compilation error

--- tournament_clean_arch/tests/test_create_tournament.py
+++ tournament_clean_arch/tests/test_create_tournament.py
@@ -57,3 +57,39 @@ class TestCreateTournament(TestCase):
 
         self.assertEqual(args_dict.get("tournament_details"),
                          tournament_data_with_date_as_obj)
+
+    def test_create_tournament_rounds_gte_3(self):
+        tournament_date_time = \
+            datetime.datetime.now() + datetime.timedelta(days=1)
+        tournament_data = {
+            "tournament_id": 1,
+            "no_of_rounds": 2,
+            "start_datetime":
+                tournament_date_time.strftime("%m/%d/%Y, %H:%M:%S")
+        }
+        tournament_data_with_date_as_obj = copy.deepcopy(tournament_data)
+        tournament_data_with_date_as_obj["start_datetime"] = \
+            tournament_date_time
+
+        storage = Mock()
+        presenter = Mock()
+
+        tournament_id = tournament_data["tournament_id"]
+        storage.create_tournament.return_value = tournament_id
+        storage.get_tournament.return_value = tournament_data_with_date_as_obj
+        presenter.present_create_tournament.return_value = tournament_data
+
+        from tournament_clean_arch.use_cases.create_tournament_interactor \
+            import CreateTournamentInteractor
+        use_case = CreateTournamentInteractor(
+            storage=storage, presenter=presenter)
+
+        use_case.setup(
+            no_of_rounds=tournament_data["no_of_rounds"],
+            start_datetime=tournament_data["start_datetime"]
+        )
+
+        from tournament_clean_arch.constants.custom_exceptions import \
+            InvalidNumnberOfRounds
+        with self.assertRaises(InvalidNumnberOfRounds):
+            use_case.execute()
