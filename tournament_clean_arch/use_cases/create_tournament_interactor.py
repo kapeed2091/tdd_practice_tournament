69c2ccb3938a1643ba6418b8809be150197345ab
rithweek <rithwik@ibhubs.co>
2019-05-14 15:54:33 +0530

    red state

---
+++ tournament_clean_arch/use_cases/create_tournament_interactor.py
@@ -0,0 +1,10 @@
+class CreateTournamentInteractor(object):
+    def __init__(self, storage, presenter):
+        self.storage = storage
+        self.presenter = presenter
+
+    def setup(self, no_of_rounds, start_datetime):
+        pass
+
+    def execute(self):
+        return
