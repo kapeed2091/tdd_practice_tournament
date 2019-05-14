0e0019c9fcfca00302ac2eaa7133abd87152fdc4
phaneeswar <phaneeswar7@gmail.com>
2019-05-14 12:52:13 +0530

    TestCase: generate_display_reports_with_status_amount_mismatch, state: green

--- display_reports/utils/display_report_utils.py
+++ display_reports/utils/display_report_utils.py
@@ -9,25 +9,30 @@ class DisplayReportUtils(object):
         payment_reports = storage.get_payment_reports(date_range=date_range,
                                                       franchise_ids=franchise_ids)
 
-        display_reports = self._get_matched_display_reports(
+        display_reports = self._get_display_reports(
             sale_reports=sale_reports, payment_reports=payment_reports)
         storage.create_display_reports(display_reports)
 
     @staticmethod
-    def _get_matched_display_reports(sale_reports, payment_reports):
+    def _get_display_reports(sale_reports, payment_reports):
         display_reports = []
         for sale_report in sale_reports:
             sale_report_ref_no = sale_report['ref_no']
             sale_report_amount = sale_report['amount']
 
             for payment_report in payment_reports:
-                if sale_report_ref_no == payment_report['ref_no'] and \
-                        sale_report_amount == payment_report['amount']:
+                if sale_report_ref_no == payment_report['ref_no']:
+                    status = DisplayReportStatus.MATCHED.value \
+                        if sale_report_amount == payment_report['amount'] else \
+                        DisplayReportStatus.AMOUNT_MISMATCH.value
+
                     display_reports.append(
                         {
-                            "ref_no": sale_report_ref_no,
-                            "amount": sale_report_amount,
-                            "status": DisplayReportStatus.MATCHED.value
+                            "sale_report_ref_no": sale_report_ref_no,
+                            "payment_report_ref_no": payment_report['ref_no'],
+                            "sale_report_amount": sale_report_amount,
+                            "payment_report_amount": payment_report['amount'],
+                            "status": status
                         }
                     )
                 break
