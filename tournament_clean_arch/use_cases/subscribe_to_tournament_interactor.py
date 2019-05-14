079a64002bd5bb7c3544ad6ae29da6eb04223279
rithweek <rithwik@ibhubs.co>
2019-05-14 17:22:08 +0530

    green state

--- tournament_clean_arch/use_cases/subscribe_to_tournament_interactor.py
+++ tournament_clean_arch/use_cases/subscribe_to_tournament_interactor.py
@@ -10,4 +10,12 @@ class SubscribeToTournamentInteractor(object):
         self.tournament_id = tournament_id
 
     def execute(self):
-        return
+        user_tournament_id = self.storage.create_user_tournament(
+            user_id=self.user_id,
+            tournament_id=self.tournament_id
+        )
+
+        presenter_details = self.presenter.present_create_user_tournament(
+            user_tournament_id=user_tournament_id
+        )
+        return presenter_details
