c5aa272a7db6e6389dcf5940dab3d44b454f058a
phaneeswar <phaneeswar7@gmail.com>
2019-05-14 12:17:45 +0530

    TestCase: generate_display_reports, state:Green

--- display_reports/utils/display_report_utils.py
+++ display_reports/utils/display_report_utils.py
@@ -1,5 +1,28 @@
+from display_reports.constants.general import DisplayReportStatus
+
 
 class DisplayReportUtils(object):
 
     def generate_display_reports(self, date_range, franchise_ids, storage):
-        pass
+        sale_reports = storage.get_sale_reports(date_range=date_range,
+                                                franchise_ids=franchise_ids)
+        payment_reports = storage.get_payment_reports(date_range=date_range,
+                                                      franchise_ids=franchise_ids)
+
+        display_reports = []
+        for sale_report in sale_reports:
+            sale_report_ref_no = sale_report['ref_no']
+            sale_report_amount = sale_report['amount']
+
+            for payment_report in payment_reports:
+                if sale_report_ref_no == payment_report['ref_no'] and \
+                        sale_report_amount == payment_report['amount']:
+                    display_reports.append(
+                        {
+                            "ref_no": sale_report_ref_no,
+                            "amount": sale_report_amount,
+                            "status": DisplayReportStatus.MATCHED.value
+                        }
+                    )
+                break
+        storage.generate_display_reports(display_reports)
