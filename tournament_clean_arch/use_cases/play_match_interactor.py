80b3f0ba3850170b1a2f8d1506d96f468655553e
rithweek <rithwik@ibhubs.co>
2019-05-14 20:36:59 +0530

    green state: test_play_match - successful case

--- tournament_clean_arch/use_cases/play_match_interactor.py
+++ tournament_clean_arch/use_cases/play_match_interactor.py
@@ -12,4 +12,16 @@ class PlayMatchInteractor(object):
         self.tournament_id = tournament_id
 
     def execute(self):
-        return
+        match_id = self.storage.get_unassigned_match_for_round(
+            round_number=self.round_number,
+            tournament_id=self.tournament_id
+        )
+
+        user_match_id = self.storage.assign_match_to_user(
+            match_id=match_id, user_id=self.user_id
+        )
+
+        presenter_details = self.presenter.present_play_match(
+            user_match_id=user_match_id
+        )
+        return presenter_details
