196eee559789bd540d2a838ef713ab46af58305b
phaneeswar <phaneeswar7@gmail.com>
2019-05-15 14:03:41 +0530

    TestCase: get_display_reports:invalid_date_range, state:Red

---
+++ display_reports/views/get_display_reports/tests/snapshots/snap_test_case_02.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase02GetDisplayReportsAPITestCase::test_case status'] = 200
+
+snapshots['TestCase02GetDisplayReportsAPITestCase::test_case body'] = {
+    'display_reports': [
+    ]
+}
+
+snapshots['TestCase02GetDisplayReportsAPITestCase::test_case header_params'] = {
+    'allow': (
+        'Allow',
+        'POST, OPTIONS'
+    ),
+    'content-language': (
+        'Content-Language',
+        'en'
+    ),
+    'content-length': (
+        'Content-Length',
+        '22'
+    ),
+    'content-type': (
+        'Content-Type',
+        'application/json'
+    ),
+    'vary': (
+        'Vary',
+        'Accept-Language, Origin, Cookie'
+    ),
+    'x-frame-options': (
+        'X-Frame-Options',
+        'SAMEORIGIN'
+    )
+}
---
+++ display_reports/views/get_display_reports/tests/test_case_02.py
@@ -0,0 +1,44 @@
+"""
+# Negative Test Case: Invalid Date Range
+"""
+from django_swagger_utils.drf_server.exceptions import BadRequest
+from django_swagger_utils.utils.test import CustomAPITestCase
+
+from display_reports.constants.general import DisplayReportStatus
+from display_reports.utils.convert_unicode_strings import convert_unicode_strings
+from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
+
+REQUEST_BODY = """
+{
+    "date_range": {
+        "from_date": "2019-03-10", 
+        "to_date": "2019-03-05"
+    }, 
+    "franchise_ids": [
+        1
+    ]
+}
+"""
+
+TEST_CASE = {
+    "request": {
+        "path_params": {},
+        "query_params": {},
+        "header_params": {},
+        "securities": {"oauth": {"scopes": ["read", "write"], "tokenUrl": "http://auth.ibtspl.com/oauth2/", "flow": "password", "type": "oauth2"}},
+        "body": REQUEST_BODY,
+    },
+}
+
+
+class TestCase02GetDisplayReportsAPITestCase(CustomAPITestCase):
+    app_name = APP_NAME
+    operation_name = OPERATION_NAME
+    request_method = REQUEST_METHOD
+    url_suffix = URL_SUFFIX
+    test_case_dict = TEST_CASE
+
+    def test_case(self):
+
+        with self.assertRaisesMessage(BadRequest, 'From date can not be greater than To date'):
+            self.default_test_case()
