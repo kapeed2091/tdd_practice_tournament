551df25d7ba6f3f73ecf95be0cf51da9b1ce83ba
phaneeswar <phaneeswar7@gmail.com>
2019-05-15 11:35:20 +0530

    TestCase: get_display_reports, state:Green

--- display_reports/presenters/preseenter_csv_impl.py
+++ display_reports/presenters/preseenter_csv_impl.py
@@ -4,4 +4,5 @@ from display_reports.presenters.presenter import Presenter
 class PresenterCsvImpl(Presenter):
 
     def get_display_reports(self, display_reports_data):
-        pass
+        # Dummy implementation
+        return "file/path/display_reports.csv"
