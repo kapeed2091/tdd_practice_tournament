21b8f0defdfd61311a7668312937cf040ed78e9e
phaneeswar <phaneeswar7@gmail.com>
2019-05-15 11:32:13 +0530

    TestCase: get_display_reports, state: Compile error

---
+++ display_reports/presenters/tests/tests_presenter_csv_impl.py
@@ -0,0 +1,17 @@
+from display_reports.constants.general import DisplayReportStatus
+
+
+def test_get_display_reports():
+    display_reports_data = [
+        {
+            "sale_report_reference_no": "Ref1",
+            "payment_report_reference_no": "Ref1",
+            "sale_report_amount": 100,
+            "payment_report_amount": 100,
+            "status": DisplayReportStatus.MATCHED.value
+        }
+    ]
+
+    presenter_csv_impl = PresenterCsvImpl()
+    file_path = presenter_csv_impl.get_display_reports(display_reports_data)
+    assert file_path is not None
