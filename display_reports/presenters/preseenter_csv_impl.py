cf888225b29fbf65058479f6588706736f25871a
phaneeswar <phaneeswar7@gmail.com>
2019-05-15 11:33:55 +0530

    TestCase: get_display_reports, state:Red

---
+++ display_reports/presenters/preseenter_csv_impl.py
@@ -0,0 +1,7 @@
+from display_reports.presenters.presenter import Presenter
+
+
+class PresenterCsvImpl(Presenter):
+
+    def get_display_reports(self, display_reports_data):
+        pass
--- display_reports/presenters/tests/tests_presenter_csv_impl.py
+++ display_reports/presenters/tests/tests_presenter_csv_impl.py
@@ -12,6 +12,7 @@ def test_get_display_reports():
         }
     ]
 
+    from display_reports.presenters.preseenter_csv_impl import PresenterCsvImpl
     presenter_csv_impl = PresenterCsvImpl()
     file_path = presenter_csv_impl.get_display_reports(display_reports_data)
     assert file_path is not None
