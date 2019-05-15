98947ffb980f0bc37b8203cc7ba892361a41059f
phaneeswar <phaneeswar7@gmail.com>
2019-05-15 16:18:55 +0530

    Implementation of generate_display_reports

--- display_reports/views/generate_display_reports/api_wrapper.py
+++ display_reports/views/generate_display_reports/api_wrapper.py
@@ -5,5 +5,16 @@ from .validator_class import ValidatorClass
 
 @validate_decorator(validator_class=ValidatorClass)
 def api_wrapper(*args, **kwargs):
-    pass
+    request_data = kwargs['request_data']
+    date_range = request_data['date_range']
+    franchise_ids = request_data['franchise_ids']
 
+    from display_reports.interactors.display_report_interactor import DisplayReportInteractor
+    from display_reports.storage.storage_impl import StorageImplementation
+
+    storage = StorageImplementation()
+    display_report_interactor = DisplayReportInteractor(
+        date_range=date_range, franchise_ids=franchise_ids, storage=storage
+    )
+    display_report_interactor.generate_display_reports()
+    return
