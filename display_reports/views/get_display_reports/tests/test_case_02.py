e8c16acbb4c24a8b726635bfd00c340295ff0441
phaneeswar <phaneeswar7@gmail.com>
2019-05-15 16:26:21 +0530

    Optimizing imports

--- display_reports/views/get_display_reports/tests/test_case_02.py
+++ display_reports/views/get_display_reports/tests/test_case_02.py
@@ -1,11 +1,7 @@
 """
 # Negative Test Case: Invalid Date Range
 """
-from django_swagger_utils.drf_server.exceptions import BadRequest
 from django_swagger_utils.utils.test import CustomAPITestCase
-
-from display_reports.constants.general import DisplayReportStatus
-from display_reports.utils.convert_unicode_strings import convert_unicode_strings
 from . import APP_NAME, OPERATION_NAME, REQUEST_METHOD, URL_SUFFIX
 
 REQUEST_BODY = """
