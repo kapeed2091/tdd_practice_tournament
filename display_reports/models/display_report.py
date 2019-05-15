7a99f7f8b91964220982e31044ce99fde494e044
phaneeswar <phaneeswar7@gmail.com>
2019-05-15 10:35:55 +0530

    Updating model

--- display_reports/models/display_report.py
+++ display_reports/models/display_report.py
@@ -3,10 +3,14 @@ from django.db import models
 
 
 class DisplayReport(AbstractDateTimeModel):
-    sale_report_reference_no = models.CharField(max_length=125)
-    payment_report_reference_no = models.CharField(max_length=125)
-    sale_report_amount = models.FloatField()
-    payment_report_amount = models.FloatField()
+    sale_report_reference_no = models.CharField(
+        max_length=125, null=True, blank=True)
+    payment_report_reference_no = models.CharField(
+        max_length=125, null=True, blank=True)
+    sale_report_amount = models.FloatField(
+        null=True, blank=True)
+    payment_report_amount = models.FloatField(
+        null=True, blank=True)
     franchise_id = models.PositiveIntegerField()
     transaction_datetime = models.DateTimeField()
     status = models.CharField(max_length=125)
